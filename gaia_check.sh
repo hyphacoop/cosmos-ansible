e1bb81fb4d9d0c3a7510a1f75d62a1540d09a073
ASoTNetworks <dev@aseriesoftubez.com>
2022-05-16 14:53:49 -0400

    test-ci

--- gaia_check.sh
+++ gaia_check.sh
@@ -4,9 +4,27 @@ set -x
 gaia_host=$1
 gaia_port=$2
 
-HEIGHT=$(curl -s http://$gaia_host:$gaia_port/block | jq -r .result.block.header.height)
-sleep 10
-HEIGHT2=$(curl -s http://$gaia_host:$gaia_port/block | jq -r .result.block.header.height)
+# Waiting until gaiad responds
+attempt_counter=0
+max_attempts=60
+until $(curl --output /dev/null --silent --head --fail http://$gaia_host:$gaia_port); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Tried connecting to gaiad for $attempt_counter times"
+      exit 1
+    fi
 
-echo "$HEIGHT"
-echo "$HEIGHT2"
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 1
+done
+
+# Checking to see if gaia is producing blocks
+height=0
+while [ $height -gt 10 ]
+do
+    height=$(curl -s http://$gaia_host:$gaia_port/block | jq -r .result.block.header.height)
+    echo "Current height is: $HEIGHT"
+    sleep 1
+done
+
+echo "Hieght is more than 10 assuming gaiad is running fine"
