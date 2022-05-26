0232191cdcd7ff2176d66d454a001fc0e1fa4c4a
Dante <dante.a.sanchez@gmail.com>
2022-05-26 13:56:01 -0400

    Flexible cutoff version

--- tests/generate_version_matrix.py
+++ tests/generate_version_matrix.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
 import json
+import sys
 import requests
+
+
+# Must provide a cutoff version, e.g. 'v6.0.4'
+starting_version = sys.argv[1]
+version_major = int(starting_version[1])
+version_patch = int(starting_version[5])
 
 # Read in releases from GitHub API
 releases_list = requests.get(
@@ -10,11 +17,12 @@ releases_list = requests.get(
 with open('releases.json', 'w', encoding='utf-8') as outfile:
     json.dump(releases_list, outfile)
 
-# Trim list to only releases from 6.0.4 onwards
-post_604_releases = [release for release in releases_list if
-                     (int(release['name'][1]) == 6 and int(release['name'][5]) == 4) or
-                     int(release['name'][1]) > 6]
+# Trim list to only releases from specified version onwards
+trimmed_releases = [release for release in releases_list if
+                    (int(release['name'][1]) == version_major and
+                     int(release['name'][5]) >= version_patch) or
+                    int(release['name'][1]) > version_major]
 
 start_json = json.dumps(
-    {'gaia_version': [rel['name'] for rel in post_604_releases]})
+    {'gaia_version': [rel['name'] for rel in trimmed_releases]})
 print(start_json)
