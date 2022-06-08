b5f4fafb269f9a64900bcdd4ad12787c67e77387
ASoTNetworks <dev@aseriesoftubez.com>
2022-06-08 14:12:01 -0400

    made export_genesis.sh more verbose

--- tests/export_genesis.sh
+++ tests/export_genesis.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
-set -x
 set -e
 
 gh_branch="export-genesis"
 gh_user="hypha-bot"
 
 # Stop cosmovisor
+echo "Stopping cosmovisor"
 systemctl stop cosmovisor
 
-# Use quicksync
+# Use quicksync as statesync is not reliable
+echo "Installing utils needed to quicksync"
 apt-get install wget liblz4-tool aria2 bc -y
 
+echo "Creating script for gaia user"
 echo "#!/bin/bash
-set -x
 set -e
+echo \"cd ~/.gaia\"
 cd ~/.gaia
-URL=\$(curl https://quicksync.io/cosmos.json|jq -r '.[] |select(.file=="cosmoshub-4-pruned")|.url')
+echo \"Set URL\"
+URL=\$(curl https://quicksync.io/cosmos.json|jq -r '.[] |select(.file==\"cosmoshub-4-pruned\")|.url')
+echo \"URL set to: \$URL\"
+echo \"Starting download\"
 aria2c -x5 \$URL
+echo \"Download checksum script\"
 wget https://raw.githubusercontent.com/chainlayer/quicksync-playbooks/master/roles/quicksync/files/checksum.sh
 chmod +x checksum.sh
+echo \"Download \$URL.checksum\"
 wget \$URL.checksum
+echo \"Get sha512sum\"
 curl -s https://lcd-cosmos.cosmostation.io/txs/\$(curl -s \$URL.hash)|jq -r '.tx.value.memo'|sha512sum -c
+echo \"Checking hash of download\"
 ./checksum.sh \$(basename \$URL) check
+echo \"Execting \$(basename \$URL)\"
 lz4 -d \$(basename \$URL) | tar xf -
+echo \"Removing \$(basename \$URL)\"
 rm \$(basename \$URL)
 if [ ! -d cosmovisor/upgrades ]
 then
+    echo \"Creating cosmovisor/upgrades/v7-Theta/bin directory\"
     mkdir -p cosmovisor/upgrades/v7-Theta/bin
     cp cosmovisor/genesis/bin/gaiad cosmovisor/upgrades/v7-Theta/bin/gaiad
 fi
 " > ~gaia/quicksync.sh
 chmod +x ~gaia/quicksync.sh
+echo "Running ~gaia/quicksync.sh as gaia user"
 su gaia -c '~gaia/quicksync.sh'
 
+echo "Starting cosmovisor"
 systemctl start cosmovisor
 
 # Wait for gaia service to respond
