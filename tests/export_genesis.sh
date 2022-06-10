aed65f15d9d2758572b98ca51e81c03985079569
ASoTNetworks <dev@aseriesoftubez.com>
2022-06-10 11:47:09 -0400

    push export log after done

--- tests/export_genesis.sh
+++ tests/export_genesis.sh
@@ -4,6 +4,9 @@ set -e
 gh_branch="export-genesis"
 gh_user="hypha-bot"
 
+# Store current date and time
+start_date=$(date +"%Y%m%d_%H-%M-%S")
+
 # Stop cosmovisor
 echo "Stopping cosmovisor"
 systemctl stop cosmovisor
@@ -32,7 +35,7 @@ echo \"Checking hash of download\"
 ./checksum.sh \$(basename \$URL) check
 if [ \$? -ne 0 ]
 then
-	echo "Checksum FAILED using statesync"
+	echo "Checksum FAILED falling back to statesync"
 	rm \$(basename \$URL)
 else
 	echo \"Execting \$(basename \$URL)\"
@@ -51,80 +54,25 @@ chmod +x ~gaia/quicksync.sh
 echo "Running ~gaia/quicksync.sh as gaia user"
 su gaia -c '~gaia/quicksync.sh'
 
-echo "Starting cosmovisor"
-systemctl start cosmovisor
 
-# Wait for gaia service to respond
-echo "Waiting for gaia to respond"
-attempt_counter=0
-max_attempts=100
-until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:26657)
-do
-    if [ ${attempt_counter} -gt ${max_attempts} ]
-    then
-        echo ""
-        echo "Tried connecting to gaiad for $attempt_counter times. Exiting."
-        exit 1
-    fi
 
-    printf '.'
-    attempt_counter=$(($attempt_counter+1))
-    sleep 1
-done
+# Print current date and time
+echo -n "Finished at: "
+date
 
-# Wait until gaiad is done catching up
-catching_up="true"
-while [ $catching_up == "true" ]
-do
-	catching_up=$(curl -s 127.0.0.1:26657/status | jq -r .result.sync_info.catching_up)
-	echo "catching up"
-	sleep 5
-done
-echo "Done catching up"
-
-# Get current block height
-current_block=$(curl -s 127.0.0.1:26657/block | jq -r .result.block.header.height)
-echo "Current block: $current_block"
-
-# Stop cosmovisor before exporting
-echo "stop cosmovisor"
-systemctl stop cosmovisor
-
-# Clone cosmos-genesis-tinkerer
-echo "Cloneing cosmos-genesis-tinkerer"
+# Push log to cosmos-configurations-private repo
+echo "Push log to cosmos-configurations-private repo"
 cd ~
-if [ ! -d ~/.ssh ]
+git clone git@github.com:hyphacoop/cosmos-configurations-private.git
+cd cosmos-configurations-private
+if [ ! -d logs/mainnet-export ]
 then
-    mkdir -m 700 ~/.ssh
+    mkdir -p logs/mainnet-export
 fi
-
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-git config --global credential.helper store
-git config --global user.name "$gh_user"
-git config --global user.email $gh_user@users.noreply.github.com
-git clone git@github.com:hyphacoop/cosmos-genesis-tinkerer.git
-cd cosmos-genesis-tinkerer/
-git checkout $gh_branch
-
-# Export genesis
-if [ ! -d mainnet-genesis-export ]
-then
-    mkdir mainnet-genesis-export
-fi
-echo "Export genesis"
-cd mainnet-genesis-export
-su gaia -c "~gaia/.gaia/cosmovisor/current/bin/gaiad export --height $current_block" 2> mainnet_genesis_$current_block.json
-echo "Compressing mainnet_genesis_$current_block.json"
-gzip mainnet_genesis_$current_block.json
-
-# Push to github
-apt install -y git-lfs
-echo "push to github"
-git lfs install
-git lfs track "*.gz"
+cp /root/export_genesis.log logs/mainnet-export/$date\_export_genesis.log
 git add -A
-git commit -m "Adding mainnet genesis at height $current_block"
-git push origin $gh_branch
+git commit -m "Adding export log file"
+git push origin main
 
 # DESTROY the droplet from itself
 curl -X DELETE -H "Authorization: Bearer {{ digitalocean_api_key }}" "https://api.digitalocean.com/v2/droplets/{{ droplet_id }}"
