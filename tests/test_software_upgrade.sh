570f9c11046e6290110929dc70ee5e518fdfd109
ASoTNetworks <dev@aseriesoftubez.com>
2022-07-10 14:59:53 -0400

    wait longer for upgrade proposal to go on chain

--- tests/test_software_upgrade.sh
+++ tests/test_software_upgrade.sh
@@ -73,7 +73,7 @@ if [ -n "$upgrade_name" ]; then
     txhash=$($proposal | jq -r .txhash)
 
     # Wait for the proposal to go on chain
-    sleep 6
+    sleep 24
 
     # Get proposal ID from txhash
     echo "Get proposal ID from txhash"
