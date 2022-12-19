03f4b6f5ab73b37ecdc5b4aeb1a3e0953eadfd97
ASoTNetworks <dev@aseriesoftubez.com>
2022-12-19 10:24:06 -0500

    use useragent for get_trust_hash and get_trust_height scripts.

--- roles/node/files/get_trust_hash.sh
+++ roles/node/files/get_trust_hash.sh
@@ -3,4 +3,4 @@
 # Retrieve hash ID
 # $1: rpc address
 # $2: block height
-echo -n $(curl -s "$1/block?height=$2" | jq -r .result.block_id.hash)
+echo -n $(curl -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0" -s "$1/block?height=$2" | jq -r .result.block_id.hash)
--- roles/node/files/get_trust_height.sh
+++ roles/node/files/get_trust_height.sh
@@ -3,6 +3,6 @@
 # Retrieve trust height
 # $1: rpc address
 INTERVAL=1000
-LATEST_HEIGHT=$(curl -s $1/block | jq -r .result.block.header.height)
+LATEST_HEIGHT=$(curl -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0" -s $1/block | jq -r .result.block.header.height)
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL))
 echo -n $BLOCK_HEIGHT
