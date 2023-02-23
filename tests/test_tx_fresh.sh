2c1515cd985dc177cae12224dade63a7e096a448
Dante <dante.a.sanchez@gmail.com>
2023-02-23 10:36:46 -0500

    500denom fee and -b block flag

--- tests/test_tx_fresh.sh
+++ tests/test_tx_fresh.sh
@@ -41,7 +41,7 @@ echo "test-account has address $test_account."
 
 # Test tx send
 echo "Sending funds from faucet to test account..."
-TXHASH=$(gaiad tx bank send $faucet $test_account 5000100$denom --from $faucet --keyring-backend test --fees 1$denom --chain-id my-testnet -y -o json | jq '.txhash' | tr -d '"')
+TXHASH=$(gaiad tx bank send $faucet $test_account 5000100$denom --from $faucet --keyring-backend test --fees 500$denom --chain-id my-testnet -y -o json -b block | jq '.txhash' | tr -d '"')
 echo $TXHASH
 echo "Waiting for transaction to go on chain..."
 sleep 6
@@ -50,7 +50,7 @@ check_code $TXHASH
 # Test delegation
 echo "Delegating funds from test account to validator..."
 # Delegate from test-account to validator
-TXHASH=$(gaiad tx staking delegate $val1 4000000$denom --from test-account --keyring-backend test --fees 1$denom --chain-id my-testnet -y -o json | jq '.txhash' | tr -d '"')
+TXHASH=$(gaiad tx staking delegate $val1 4000000$denom --from test-account --keyring-backend test --fees 500$denom --chain-id my-testnet -y -o json -b block | jq '.txhash' | tr -d '"')
 echo "Waiting for transaction to go on chain..."
 sleep 12
 check_code $TXHASH
@@ -60,7 +60,7 @@ starting_balance=$(gaiad q bank balances $test_account -o json | jq -r '.balance
 balance_denom=$(gaiad q bank balances $test_account -o json | jq -r '.balances[0].denom')
 
 echo "Withdrawing rewards for test account..."
-TXHASH=$(gaiad tx distribution withdraw-all-rewards --from $test_account --keyring-backend test --fees 1$denom --chain-id my-testnet -y -o json | jq '.txhash' | tr -d '"')
+TXHASH=$(gaiad tx distribution withdraw-all-rewards --from $test_account --keyring-backend test --fees 500$denom --chain-id my-testnet -y -o json -b block | jq '.txhash' | tr -d '"')
 # Wait for rewards to accumulate
 echo "Waiting for transaction to go on chain..."
 sleep 6
