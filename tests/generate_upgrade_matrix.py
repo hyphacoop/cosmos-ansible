55f3da4e963f280010c887e585ae4fa4da2fe32e
Dante <dante.a.sanchez@gmail.com>
2023-03-02 22:24:16 -0500

    Add skip_versions to upgrade matrix

--- tests/generate_upgrade_matrix.py
+++ tests/generate_upgrade_matrix.py
@@ -3,6 +3,8 @@
 import json
 import sys
 
+SKIP_VERSIONS = ['v8.0.0-rc',   # software-upgrade command not available
+                 'v8.0.0-rc1']  # unsuccessful upgrade to v9.0.0-rc3 through v9.0.0: https://github.com/hyphacoop/cosmos-ansible/actions/runs/4319476707
 
 # Must provide a cutoff version, e.g. 'v6.0.4'
 starting_version = sys.argv[1]
@@ -19,13 +21,17 @@ trimmed_releases = [release for release in releases_list if
                      int(release['name'][5]) >= version_patch) or
                     int(release['name'][1]) > version_major]
 
+# Trim list further to remove all releases listed in the SKIP_VERSIONS list
+filtered_releases = [release for release in trimmed_releases if
+                     release['name'] not in SKIP_VERSIONS ]
+
 # Set upgrade versions to target for each release
-matrix = {release['name']: [] for release in trimmed_releases}
+matrix = {release['name']: [] for release in filtered_releases}
 
 for start_version, _ in matrix.items():
     matrix[start_version] = [
         release['name']
-        for release in trimmed_releases
+        for release in filtered_releases
         if int(release['name'][1]) > int(start_version[1])]
 
 # Assemble matrix include section:
