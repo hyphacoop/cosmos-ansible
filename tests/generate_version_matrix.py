283adefca51d665bbc345021b03745c2536feb13
ASoTNetworks <dev@aseriesoftubez.com>
2023-05-02 10:50:06 -0400

    restore skip version

--- tests/generate_version_matrix.py
+++ tests/generate_version_matrix.py
@@ -4,10 +4,8 @@ import json
 import sys
 import requests
 
-#SKIP_VERSIONS = ['v8.0.0-rc',   # software-upgrade command not available
-#                 'v8.0.0-rc1']  # unsuccessful upgrade to v9.0.0-rc3 through v9.0.0: https://github.com/hyphacoop/cosmos-ansible/actions/runs/4319476707
-
-SKIP_VERSIONS = []
+SKIP_VERSIONS = ['v8.0.0-rc',   # software-upgrade command not available
+                 'v8.0.0-rc1']  # unsuccessful upgrade to v9.0.0-rc3 through v9.0.0: https://github.com/hyphacoop/cosmos-ansible/actions/runs/4319476707
 
 # Must provide a cutoff version, e.g. 'v6.0.4'
 starting_version = sys.argv[1].split('.')
@@ -39,10 +37,10 @@ for name in release_names:
         else:
             releases.append(name)
 
-# # Remove all rcs from the list if there is a final release available
-# for rc in rc_releases:
-#     if rc.split('-')[0] not in releases:
-#         releases.append(rc)
+# Remove all rcs from the list if there is a final release available
+for rc in rc_releases:
+    if rc.split('-')[0] not in releases:
+        releases.append(rc)
 
 # Trim list further to remove all releases listed in the SKIP_VERSIONS list
 filtered_releases = [release for release in releases if
