7236589909956a712c59be5815521738f5418ccc
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-06-09 15:01:02 -0400

    filter releases for patch upgrade

--- tests/patch_upgrade/patch_upgrade_matrix.py
+++ tests/patch_upgrade/patch_upgrade_matrix.py
@@ -10,9 +10,13 @@ import requests
 gaia_releases = requests.get(
     'https://api.github.com/repos/cosmos/gaia/releases', timeout=30).json()
 
-for release in gaia_releases:
-    if 'rc' not in release['name']:
-        start_version = release['name']
+# Filter names that do not comply to `vA.B.C` format (-rcX suffix is fine)
+version_pattern = r'v\d+.\d+.\d+(-rc\d*)?'
+release_names = [ release['name'] for release in releases_list if re.fullmatch(version_pattern, release['name']) ]
+
+for name in release_names:
+    if 'rc' not in name:
+        start_version = name
         version_parts = start_version.split('.')
         start_major_version = int(version_parts[0][1:])
         start_minor_version = int(version_parts[1])
@@ -20,31 +24,27 @@ for release in gaia_releases:
         break
 
 target_rc_version = ''
-for release in gaia_releases:
-    if 'rc' in release['name']:
-        version_parts =  release['name'].split('.')
+for release in release_names:
+    if 'rc' in name:
+        version_parts =  name.split('.')
         rc_major_version = int(version_parts[0][1:])
         rc_minor_version = int(version_parts[1])
         rc_patch_version = int(version_parts[2].split('-')[0])
         if (rc_major_version == start_major_version) and \
            ((rc_minor_version == start_minor_version) and \
            (rc_patch_version > start_patch_version)):
-            target_rc_version = release['name']
+            target_rc_version = name
             break
 
 # Assemble matrix include section:
 includes = [
     {'start_version': start_version, 'target_branch': f'release-v{start_major_version}', 'upgrade_coverage': 'full'},
     {'start_version': start_version, 'target_branch': f'release-v{start_major_version}', 'upgrade_coverage': 'partial'},
-    {'start_version': start_version, 'target_branch': 'main', 'upgrade_coverage': 'full'},
-    {'start_version': start_version, 'target_branch': 'main', 'upgrade_coverage': 'partial'},
 ]
 
 if target_rc_version:
     includes.append({'start_version': start_version, 'target_branch': target_rc_version, 'upgrade_coverage': 'full'})
     includes.append({'start_version': start_version, 'target_branch': target_rc_version, 'upgrade_coverage': 'partial'})
-
-
 
 upgrade_json = json.dumps({'include': includes})
 print(upgrade_json)
