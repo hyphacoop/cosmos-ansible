11abace20f89803be2f0bb602ebf485f5db87eed
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-06-12 13:52:55 -0400

    gas prices proposal

--- tests/v11_upgrade/set_minimum_gas_prices.sh
+++ tests/v11_upgrade/set_minimum_gas_prices.sh
@@ -3,7 +3,7 @@
 validator_address=$(jq -r '.address' ~/.gaia/validator.json)
 
 echo "Submitting proposal to update the minimum gas prices..."
-proposal="gaiad tx gov submit-proposal param-change tests/v11_upgrade/min_gas_prices_proposal.json --from $validator_address --gas auto --fees 500uatom -b block -y -o json"
+proposal="gaiad tx gov submit-proposal param-change tests/v11_upgrade/min_gas_prices_proposal.json --from $WALLET_1 --gas auto --fees $BASE_FEES$DENOM -b block -y -o json --home $HOME_1"
 echo $proposal
 txhash=$($proposal | jq -r .txhash)
 # Wait for the proposal to go on chain
@@ -11,11 +11,11 @@ sleep 6
 
 # Get proposal ID from txhash
 echo "Get proposal ID from txhash"
-proposal_id=$(gaiad --output json q tx $txhash | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+proposal_id=$(gaiad --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
 
 # Vote yes on the proposal
 echo "Submitting the \"yes\" vote to proposal $proposal_id"
-vote="gaiad tx gov vote $proposal_id yes --from $validator_address --fees 500uatom --yes"
+vote="gaiad tx gov vote $proposal_id yes --from $WALLET_1 --gas auto --fees $BASE_FEES$DENOM -b block --yes --home $HOME_1"
 echo $vote
 $vote
 sleep 6
