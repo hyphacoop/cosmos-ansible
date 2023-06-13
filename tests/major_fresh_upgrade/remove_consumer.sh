1ad3f0efcf16dbe5f70bcfc8f70d1aa628527a6d
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-06-13 10:48:38 -0400

    gas adjustment for consumer-removal prop

--- tests/major_fresh_upgrade/remove_consumer.sh
+++ tests/major_fresh_upgrade/remove_consumer.sh
@@ -8,17 +8,18 @@ sed "s%\"chain_id\": \"\"%\"chain_id\": \"$CONSUMER_CHAIN_ID\"%g" proposal-remov
 rm proposal-remove.json
 
 echo "Passing proposal..."
-proposal="$CHAIN_BINARY tx gov submit-proposal consumer-removal proposal-remove-$CONSUMER_CHAIN_ID.json --home $HOME_1 --gas auto --fees 1000$DENOM --from $WALLET_1 --keyring-backend test --chain-id $CHAIN_ID -b block -y -o json"
+proposal="$CHAIN_BINARY tx gov submit-proposal consumer-removal proposal-remove-$CONSUMER_CHAIN_ID.json --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees 1000$DENOM --from $WALLET_1 --keyring-backend test --chain-id $CHAIN_ID -b block -y -o json"
 echo $proposal
 txhash=$($proposal | jq -r .txhash)
 # Wait for the proposal to go on chain
 sleep 6
 
 # Get proposal ID from txhash
-echo "Get proposal ID from txhash"
+echo "Getting proposal ID from txhash..."
+$CHAIN_BINARY q tx $txhash --home $HOME_1
 proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
 
-$CHAIN_BINARY tx gov vote $proposal_id yes --home $HOME_1 --gas auto --fees $BASE_FEES$DENOM --from $WALLET_1  --keyring-backend test --chain-id $CHAIN_ID -b block -y
+$CHAIN_BINARY tx gov vote $proposal_id yes --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --from $WALLET_1  --keyring-backend test --chain-id $CHAIN_ID -b block -y
 
 echo "Waiting for proposal to pass and chain to stop..."
 sleep 30
