975bba776cc5d73630450244ba3adcf3c86bdbcb
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-06-13 08:46:23 -0400

    gas flags

--- tests/patch_upgrade/patch_test_tx.sh
+++ tests/patch_upgrade/patch_test_tx.sh
@@ -13,7 +13,7 @@ check_code()
 }
 
 echo "Sending funds with tx bank send..."
-TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 $VAL_STAKE_STEP$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas auto --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
+TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 $VAL_STAKE_STEP$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
 check_code $TXHASH
 sleep 10
 
