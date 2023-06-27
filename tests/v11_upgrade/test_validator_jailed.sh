b1841060bb1ad362fa743795ea6ddbbddaee13c9
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-06-27 07:23:12 -0400

    print current consumer block

--- tests/v11_upgrade/test_validator_jailed.sh
+++ tests/v11_upgrade/test_validator_jailed.sh
@@ -4,6 +4,7 @@ valoper=$1
 jailed_expected=$2
 # Check if validator is jailed or not
 echo "Verifying jailed status for validator $valoper..."
+$CHAIN_BINARY q staking validator $valoper --home $HOME_1
 jailed_actual=$($CHAIN_BINARY q staking validator $valoper --home $HOME_1 -o json | jq -r '.jailed')
 echo "Expected jailed status: $jailed_expected, actual status: $jailed_actual"
 if [ $jailed_expected != $jailed_actual ]; then
