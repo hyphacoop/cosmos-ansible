c1dd9097bf94253508f28a616658d4a20863e1b5
ASoTNetworks <dev@aseriesoftubez.com>
2023-07-07 14:35:18 -0400

    clean up test scripts

--- tests/major_stateful_upgrade/test_tx.sh
+++ tests/major_stateful_upgrade/test_tx.sh
@@ -18,31 +18,32 @@ check_code $TXHASH
 sleep 10
 
 echo "Delegating funds from test account to validator..."
-TXHASH=$($CHAIN_BINARY tx staking delegate $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
+TXHASH=$($CHAIN_BINARY tx staking delegate $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_2 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
 check_code $TXHASH
 sleep 10
 
 # Wait for rewards to accumulate
+echo "Waiting for rewards to accumulate"
 sleep 20
 echo "Withdrawing rewards for test account..."
-starting_balance=$($CHAIN_BINARY q bank balances $WALLET_1 --home $HOME_1 -o json | jq -r '.balances[] | select(.denom=="uatom").amount')
+starting_balance=$($CHAIN_BINARY q bank balances $WALLET_2 --home $HOME_1 -o json | jq -r '.balances[] | select(.denom=="uatom").amount')
 echo "Starting balance: $starting_balance"
-TXHASH=$($CHAIN_BINARY tx distribution withdraw-rewards $VALOPER_1 --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
+TXHASH=$($CHAIN_BINARY tx distribution withdraw-rewards $VALOPER_1 --home $HOME_1 --from $MONIKER_2 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
 check_code $TXHASH
+sleep 10
 
 # Check the funds again
-echo $($CHAIN_BINARY q bank balances $WALLET_1 --home $HOME_1 -o json)
-ending_balance=$($CHAIN_BINARY q bank balances $WALLET_1 --home $HOME_1 -o json | jq -r '.balances[] | select(.denom=="uatom").amount')
+echo $($CHAIN_BINARY q bank balances $WALLET_2 --home $HOME_1 -o json)
+$CHAIN_BINARY q bank balances $WALLET_2 --home $HOME_1
+ending_balance=$($CHAIN_BINARY q bank balances $WALLET_2 --home $HOME_1 -o json | jq -r '.balances[] | select(.denom=="uatom").amount')
 echo "Ending balance: $ending_balance"
 delta=$[ $ending_balance - $starting_balance]
 if [ $delta -gt 0 ]; then
     echo "$delta uatom were withdrawn successfully."
 else
-    echo "Rewards could not be withdrawn."
+    echo "Rewards could not be withdrawn. Delta is: $delta"
     exit 1
 fi
-
-$CHAIN_BINARY q staking validators --home $HOME_1
 
 echo "Unbonding funds from test account to validator..."
 TXHASH=$($CHAIN_BINARY tx staking unbond $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $HIGH_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b block | jq '.txhash' | tr -d '"')
