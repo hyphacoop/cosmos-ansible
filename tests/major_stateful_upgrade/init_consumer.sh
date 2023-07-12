7ab6d5adbd70164d2267e49057a57bbf0078de3b
ASoTNetworks <dev@aseriesoftubez.com>
2023-07-12 17:05:52 -0400

    start consumer1

---
+++ tests/major_stateful_upgrade/init_consumer.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Initialize a consumer chain
+
+echo "Running with $CONSUMER_CONSUMER_CHAIN_BINARY."
+
+# Initialize home directories
+echo "Initializing consumer homes..."
+$CONSUMER_CONSUMER_CHAIN_BINARY config chain-id $CONSUMER_CHAIN_ID --home $CONSUMER_HOME_1
+$CONSUMER_CONSUMER_CHAIN_BINARY config keyring-backend test --home $CONSUMER_HOME_1
+$CONSUMER_CONSUMER_CHAIN_BINARY config broadcast-mode block --home $CONSUMER_HOME_1
+$CONSUMER_CHAIN_BINARY config node tcp://localhost:$CON1_RPC_PORT --home $CONSUMER_HOME_1
+$CONSUMER_CONSUMER_CHAIN_BINARY init $MONIKER_1 --chain-id $CONSUMER_CHAIN_ID --home $CONSUMER_HOME_1
+
+echo "Copying keys from provider nodes to consumer ones..."
+cp $HOME_1/config/priv_validator_key.json $CONSUMER_HOME_1/config/priv_validator_key.json
+cp $HOME_1/config/node_key.json $CONSUMER_HOME_1/config/node_key.json
+
+# Update genesis file with right denom
+sed -i s%stake%$CONSUMER_DENOM%g $CONSUMER_HOME_1/config/genesis.json
+
+# Create self-delegation accounts
+echo $MNEMONIC_1 | $CONSUMER_CONSUMER_CHAIN_BINARY keys add $MONIKER_1 --keyring-backend test --home $CONSUMER_HOME_1 --recover
+
+# Add funds to accounts
+$CONSUMER_CONSUMER_CHAIN_BINARY add-genesis-account $MONIKER_1 $VAL_FUNDS$CONSUMER_DENOM --home $CONSUMER_HOME_1
+
+echo "Patching config files..."
+# app.toml
+# minimum_gas_prices
+sed -i -e "/minimum-gas-prices =/ s^= .*^= \"0$CONSUMER_DENOM\"^" $CONSUMER_HOME_1/config/app.toml
+
+# Enable API
+toml set --toml-path $CONSUMER_HOME_1/config/app.toml api.enable true
+
+# Set different ports for api
+toml set --toml-path $CONSUMER_HOME_1/config/app.toml api.address "tcp://0.0.0.0:$CON1_API_PORT"
+
+# Set different ports for grpc
+toml set --toml-path $CONSUMER_HOME_1/config/app.toml grpc.address "0.0.0.0:$CON1_GRPC_PORT"
+
+# Turn off grpc web
+toml set --toml-path $CONSUMER_HOME_1/config/app.toml grpc-web.enable false
+
+# config.toml
+# Set different ports for rpc
+toml set --toml-path $CONSUMER_HOME_1/config/config.toml rpc.laddr "tcp://0.0.0.0:$CON1_RPC_PORT"
+
+# Set different ports for rpc pprof
+toml set --toml-path $CONSUMER_HOME_1/config/config.toml rpc.pprof_laddr "localhost:$CON1_PPROF_PORT"
+
+# Set different ports for p2p
+toml set --toml-path $CONSUMER_HOME_1/config/config.toml p2p.laddr "tcp://0.0.0.0:$CON1_P2P_PORT"
+
+# Allow duplicate IPs in p2p
+toml set --toml-path $CONSUMER_HOME_1/config/config.toml p2p.allow_duplicate_ip true
+
+echo "Setting up services..."
+echo "Creating script for $CONSUMER_CHAIN_BINARY"
+echo "while true; do $HOME/go/bin/$CONSUMER_CHAIN_BINARY start --home $HOME_1; sleep 1; done" > $HOME/$CONSUMER_SERVICE_1.sh
+chmod +x $HOME/$CONSUMER_SERVICE_1.sh
+
+# Run service in screen session
+mkdir $HOME/artifact
+echo "Starting $CONSUMER_CHAIN_BINARY"
+screen -L -Logfile $HOME/artifact/$CONSUMER_SERVICE_1.log -S $CONSUMER_SERVICE_1 -d -m bash $HOME/$CONSUMER_SERVICE_1.sh
+# set screen to flush log to 0
+screen -r $CONSUMER_SERVICE_1 -p0 -X logfile flush 0
