3304e229647b06c5b42730dac1a31ce2d1fab2e6
ASoTNetworks <dev@aseriesoftubez.com>
2023-07-14 15:02:42 -0400

    test_ccv

--- tests/patch_upgrade/patch_test_ccv.sh
+++ tests/patch_upgrade/patch_test_ccv.sh
@@ -3,14 +3,16 @@
 
 # Delegate additional stake to val 1
 echo "Delegating additional stake to $MONIKER_1..."
-$CHAIN_BINARY --home $HOME_1 tx staking delegate $VALOPER_1 $VAL_STAKE_STEP$DENOM --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees 1000$DENOM -b block -y
+$CHAIN_BINARY --home $HOME_1 tx staking delegate $VALOPER_1 $VAL_STAKE_STEP$DENOM --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees 1000$DENOM -b block -y --chain-id $CHAIN_ID
 # Wait for consumer chain to get validator set update
 echo "Waiting for the validator set update to reach the consumer chain..."
 sleep 60
-PROVIDER_POWER=$(curl -s http://localhost:$VAL1_RPC_PORT/validators | jq -r '.result.validators[0].voting_power')
+PROVIDER_BADDRESS=$(jq -r '.address' $HOME_1/config/priv_validator_key.json)
+PROVIDER_POWER=$(curl -s http://localhost:$VAL1_RPC_PORT/validators | jq -r '.result.validators[] | select(.address=="'$PROVIDER_BADDRESS'") | '.voting_power'')
+
 # Verify new voting power in consumer chain
-curl -s http://localhost:$CON1_RPC_PORT/validators
-CONSUMER_POWER=$(curl -s http://localhost:$CON1_RPC_PORT/validators | jq -r '.result.validators[0].voting_power')
+CONSUMER_POWER=$(curl -s http://localhost:$CON1_RPC_PORT/validators | jq -r '.result.validators[] | select(.address=="'$PROVIDER_BADDRESS'") | '.voting_power'')
+
 echo "Top validator VP: $PROVIDER_POWER (provider), $CONSUMER_POWER (consumer)"
 if [ $PROVIDER_POWER != $CONSUMER_POWER ]; then
     echo "Consumer chain validator set does not match the provider's."
