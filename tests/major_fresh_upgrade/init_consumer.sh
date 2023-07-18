5c9e3e4efb0426a5ba49bd9a81bdb8e890f0443b
ASoTNetworks <dev@aseriesoftubez.com>
2023-07-18 19:06:20 -0400

    debug new and old add-genesis-account commands

--- tests/major_fresh_upgrade/init_consumer.sh
+++ tests/major_fresh_upgrade/init_consumer.sh
@@ -22,12 +22,13 @@ sed -i s%stake%$CONSUMER_DENOM%g $CONSUMER_HOME_1/config/genesis.json
 echo $MNEMONIC_1 | $CONSUMER_CHAIN_BINARY keys add $MONIKER_1 --keyring-backend test --home $CONSUMER_HOME_1 --recover
 
 # Add funds to accounts
-if [ ! $CONSUMER_NEW ]
+if [ $CONSUMER_NEW ]
 then
-    $CONSUMER_CHAIN_BINARY add-genesis-account $MONIKER_1 $VAL_FUNDS$CONSUMER_DENOM --home $CONSUMER_HOME_1
-else
-    # Add funds to accounts
+    echo "Running: $CONSUMER_CHAIN_BINARY genesis add-genesis-account $MONIKER_1 $VAL_FUNDS$CONSUMER_DENOM --home $CONSUMER_HOME_1"
     $CONSUMER_CHAIN_BINARY genesis add-genesis-account $MONIKER_1 $VAL_FUNDS$CONSUMER_DENOM --home $CONSUMER_HOME_1
+else
+    echo "Running: $CONSUMER_CHAIN_BINARY add-genesis-account $MONIKER_1 $VAL_FUNDS$CONSUMER_DENOM --home $CONSUMER_HOME_1"
+    $CONSUMER_CHAIN_BINARY add-genesis-account $MONIKER_1 $VAL_FUNDS$CONSUMER_DENOM --home $CONSUMER_HOME_1
 fi
 
 echo "Patching config files..."
