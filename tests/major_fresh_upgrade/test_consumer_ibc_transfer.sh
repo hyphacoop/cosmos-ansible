4729b35041106c9f106e4597c6ffc5e74b7c00fc
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-04 11:29:53 -0400

    consumer ibc transfer

--- tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
+++ tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
@@ -5,12 +5,13 @@ echo "Running with $CONSUMER_CHAIN_BINARY."
 
 PROVIDER_CHANNEL=$1
 EXPECTED_DENOMS=$2
+
 # Transfer provider token to consumer chain
 echo "Sending $DENOM to $CONSUMER_CHAIN_ID..."
-$CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer $PROVIDER_CHANNEL $WALLET_1 1000$DENOM --from $WALLET_1 --keyring-backend test --gas 500000 --fees 2000$DENOM -b block -y
+$CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer $PROVIDER_CHANNEL $RECIPIENT 1000$DENOM --from $WALLET_1 --keyring-backend test --gas 500000 --fees 2000$DENOM -b block -y
 echo "Waiting for the transfer to reach the consumer chain..."
 sleep 10
-DENOM_AMOUNT=$($CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $WALLET_1 -o json | jq -r '.balances | length')
+DENOM_AMOUNT=$($CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $RECIPIENT -o json | jq -r '.balances | length')
 if [ $DENOM_AMOUNT -lt 2 ]; then
     echo "Only one denom found in consumer wallet."
     exit 1
@@ -19,7 +20,7 @@ echo "Found at least two denoms in the consumer wallet."
 
 # Transfer consumer token to provider chain
 echo "Sending $CONSUMER_DENOM to $CHAIN_ID..."
-$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $WALLET_1 1000$CONSUMER_DENOM --from $WALLET_1 --keyring-backend test -y
+$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $WALLET_1 1000$CONSUMER_DENOM --from $RECIPIENT --keyring-backend test -y --gas auto --gas-adjustment 1.2 --fees 100$CONSUMER_FEES
 echo "Waiting for the transfer to reach the provider chain..."
 sleep 20
 $CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json
