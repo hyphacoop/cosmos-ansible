46d467bd8c842e0d92d8a7e05a69bc92f591846b
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-04 23:45:06 -0400

    tx fees

--- tests/v12_upgrade/lsm_tokenize.sh
+++ tests/v12_upgrade/lsm_tokenize.sh
@@ -6,7 +6,7 @@ sleep 2
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
 
 echo "Tokenizing shares with $WALLET_3..."
-$CHAIN_BINARY tx staking tokenize-share $VALOPER_1 50000000$DENOM --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y | jq '.'
+$CHAIN_BINARY tx staking tokenize-share $VALOPER_1 50000000$DENOM $WALLET_3 --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y | jq '.'
 sleep 2
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
 $CHAIN_BINARY q bank balances $WALLET_3 --home $HOME_1 -o json | jq '.'
--- tests/v12_upgrade/lsm_validator_bond.sh
+++ tests/v12_upgrade/lsm_validator_bond.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Delegating with $WALLET_2..."
-$CHAIN_BINARY tx staking delegate $VALOPER_1 100000000$DENOM --from $WALLET_2 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y | jq '.'
+$CHAIN_BINARY tx staking delegate $VALOPER_1 100000000$DENOM --from $WALLET_2 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y | jq '.'
 sleep 2
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
-$CHAIN_BINARY tx staking validator-bond $VALOPER_1 --from $WALLET_2 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y | jq '.'
+$CHAIN_BINARY tx staking validator-bond $VALOPER_1 --from $WALLET_2 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y --fees $BASE_FEES$DENOM | jq '.'
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
