a9c81029c532c5bf59a0936917a8a1e420aa35dd
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-04 23:16:25 -0400

    crisis module denom

--- tests/major_fresh_upgrade/start_chain.sh
+++ tests/major_fresh_upgrade/start_chain.sh
@@ -37,7 +37,8 @@ echo $MNEMONIC_5 | $CHAIN_BINARY keys add $MONIKER_5 --keyring-backend test --ho
 
 # Update genesis file with right denom
 echo "Setting denom to $DENOM..."
-jq -r --arg denom "$DENOM" '.app_state.crisis.denom |= $denom' $HOME_1/config/genesis.json > crisis.json
+cat $HOME_1/config/genesis.json
+jq -r --arg denom "$DENOM" '.app_state.crisis.constant_fee.denom |= $denom' $HOME_1/config/genesis.json > crisis.json
 jq -r --arg denom "$DENOM" '.app_state.gov.deposit_params.min_deposit.denom |= $denom' crisis.json > min_deposit.json
 jq -r --arg denom "$DENOM" '.app_state.mint.params.mint_denom |= $denom' min_deposit.json > mint.json
 cp mint.json $HOME_1/config/genesis.json
