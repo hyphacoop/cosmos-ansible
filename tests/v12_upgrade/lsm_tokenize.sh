3c8153e5402cf17ebdbf806d28a6b5aad6e4fd5c
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-04 23:51:05 -0400

    tx fees

--- tests/v12_upgrade/lsm_tokenize.sh
+++ tests/v12_upgrade/lsm_tokenize.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Delegating with $WALLET_3..."
-$CHAIN_BINARY tx staking delegate $VALOPER_1 100000000$DENOM --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y | jq '.'
+$CHAIN_BINARY tx staking delegate $VALOPER_1 100000000$DENOM --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y | jq '.'
 sleep 2
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
 
 echo "Tokenizing shares with $WALLET_3..."
-$CHAIN_BINARY tx staking tokenize-share $VALOPER_1 50000000$DENOM $WALLET_3 --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y | jq '.'
+$CHAIN_BINARY tx staking tokenize-share $VALOPER_1 50000000$DENOM $WALLET_3 --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y | jq '.'
 sleep 2
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
 $CHAIN_BINARY q bank balances $WALLET_3 --home $HOME_1 -o json | jq '.'
