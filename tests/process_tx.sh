5bfb084697c541f27135c7590f4c92c51c1c430c
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-06 20:30:06 -0400

    tx checker

---
+++ tests/process_tx.sh
@@ -0,0 +1,21 @@
+check_code()
+{
+  txhash=$1
+  code=$($CHAIN_BINARY q tx $txhash -o json | jq '.code')
+  if [ $code -eq 0 ]
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+submit_tx()
+{
+    hash=$($1 | jq -r '.txhash')
+    check=$(check_code $hash)
+    if [ check -eq 1 ]; then
+      printf "Transaction failed:\n$1\n"
+      exit 1
+    fi
+}
--- tests/v12_upgrade/lsm_redeem.sh
+++ tests/v12_upgrade/lsm_redeem.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+source tests/process_tx.sh
+
 tokenized_denom="$VALOPER_1/1"
 
 echo "Sending tokens from $WALLET_3 to $WALLET_4 via bank send..."
-$CHAIN_BINARY tx bank send $WALLET_3 $WALLET_4 20000000$tokenized_denom --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2  --fees $BASE_FEES$DENOM -b block -y | jq '.'
+submit_tx "$CHAIN_BINARY tx bank send $WALLET_3 $WALLET_4 20000000$tokenized_denom --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2  --fees $BASE_FEES$DENOM -b block -y"
 sleep 2
-# echo "Sending tokens from $WALLET_3 to $WALLET_5 via ibc transfer..."
-# $CHAIN_BINARY tx ibc-transfer transfer transfer channel-1 $WALLET_5 10000000$tokenized_denom --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2  --fees $BASE_FEES$DENOM -b block -y | jq '.'
-# sleep 2
+echo "Sending tokens from $WALLET_3 to $WALLET_5 via ibc transfer..."
+submit_tx "$CHAIN_BINARY tx ibc-transfer transfer transfer channel-1 $WALLET_5 10000000$tokenized_denom --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -b block -y"
+echo "Waiting for IBC tokens to reach $STRIDE_CHAIN_ID..."
+check_code $txhash
+sleep 10
 
 echo "Redeeming tokens from $WALLET_3..."
-$CHAIN_BINARY tx staking redeem-tokens 20000000$tokenized_denom --from $WALLET_3 --home $HOME_1 -o json --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -b block -y | jq '.'
+submit_tx "$CHAIN_BINARY tx staking redeem-tokens 20000000$tokenized_denom --from $WALLET_3 --home $HOME_1 -o json --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -b block -y"
 echo "Redeeming tokens from $WALLET_4..."
-$CHAIN_BINARY tx staking redeem-tokens 20000000$tokenized_denom --from $WALLET_4 --home $HOME_1 -o json --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -b block -y | jq '.'
-# echo "Transferring $WALLET_5 IBC tokens to LSM chain with..."
-# ibc_denom=ibc/$($CONSUMER_CHAIN_BINARY q ibc-transfer denom-hash transfer/channel-1/$tokenized_denom --home ~/.cona1 -o json | jq -r '.hash')
-# echo "IBC denom: $ibc_denom"
-# $CONSUMER_CHAIN_BINARY tx ibc-transfer transfer transfer channel-1 $WALLET_5 10000000$tokenized_denom --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2  --fees $CONSUMER_FEES$DENOM -b block -y | jq '.'
-# echo "Redeeming tokens from $WALLET_5..."
-# $CHAIN_BINARY tx staking redeem-tokens 10000000$tokenized_denom --from $WALLET_5 --home $HOME_1 -o json --gas auto --gas-adjustment --fees $BASE_FEES$DENOM -b block -y | jq '.'
+submit_tx "$CHAIN_BINARY tx staking redeem-tokens 20000000$tokenized_denom --from $WALLET_4 --home $HOME_1 -o json --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -b block -y"
+echo "Transferring $WALLET_5 IBC tokens to LSM chain with..."
+ibc_denom=ibc/$($CONSUMER_CHAIN_BINARY q ibc-transfer denom-hash transfer/channel-1/$tokenized_denom --home ~/.stride1 -o json | jq -r '.hash')
+echo "IBC denom: $ibc_denom"
+echo "Sending tokens from $WALLET_5 to $CHAIN_ID for redeem operation..."
+submit_tx "$CONSUMER_CHAIN_BINARY tx ibc-transfer transfer transfer channel-1 $WALLET_5 10000000$tokenized_denom --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2  --fees $CONSUMER_FEES$DENOM -b block -y"
+echo "Waiting for IBC tokens to reach $CHAIN_ID..."
+sleep 10
+echo "Redeeming tokens from $WALLET_5..."
+submit_tx "$CHAIN_BINARY tx staking redeem-tokens 10000000$tokenized_denom --from $WALLET_5 --home $HOME_1 -o json --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -b block -y"
+
