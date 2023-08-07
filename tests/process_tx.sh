3948628546dfbbea9faca96c96e94bac83cd2b72
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-06 22:24:07 -0400

    hash check

--- tests/process_tx.sh
+++ tests/process_tx.sh
@@ -22,7 +22,7 @@ submit_tx()
     full_tx="$2 $1 --home $3"
     echo $full_tx
     hash=$($full_tx | jq -r '.txhash')
-    check_hash $hash $2 $3)
+    check_hash $hash $2 $3
     if [[ $? -eq 1 ]]; then
       printf "Transaction failed:\n$1\n"
       exit 1
