2e09978594f3442c1a740b9f780f6a45ae9ff706
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-06 21:26:46 -0400

    redeem ops

--- tests/process_tx.sh
+++ tests/process_tx.sh
@@ -21,7 +21,7 @@ submit_tx()
     full_tx="$2 $1 --home $3"
     echo $full_tx
     hash=$($full_tx | jq -r '.txhash')
-    check=$(check_hash $2 $hash $3)
+    check=$(check_hash $hash $2 $3)
     if [ check -eq 1 ]; then
       printf "Transaction failed:\n$1\n"
       exit 1
