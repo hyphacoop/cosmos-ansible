368f772ad48d8882e504f95dad25fdacb1cf99a1
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-06 22:06:44 -0400

    jq syntax

--- tests/process_tx.sh
+++ tests/process_tx.sh
@@ -4,12 +4,14 @@ check_hash()
   # $2: binary
   # $3: home folder
   txhash=$1
-  code=$($2 q tx $txhash -o json --home $3 | jq '.code')
+  code=$($2 q tx $txhash -o json --home $3 | jq -r '.code')
   $2 q tx $txhash -o json --home $3
   if [ $code -eq 0 ]
   then
+    echo "Successful transaction."
     return 0
   else
+    echo "Failed transaction."
     return 1
   fi
 }
@@ -23,7 +25,6 @@ submit_tx()
     echo $full_tx
     hash=$($full_tx | jq -r '.txhash')
     check=$(check_hash $hash $2 $3)
-    echo "The return code is $check"
     if [[ $check -eq 1 ]]; then
       printf "Transaction failed:\n$1\n"
       exit 1
