5d8f7d8034842e05172f97b5b2cb123eff8f77be
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-08 09:46:30 -0400

    update validator bond test

--- tests/v12_upgrade/lsm_validator_bond.sh
+++ tests/v12_upgrade/lsm_validator_bond.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+source tests/process_tx.sh
+delegation=100000000
+
 echo "Delegating with $WALLET_2..."
-$CHAIN_BINARY tx staking delegate $VALOPER_1 100000000$DENOM --from $WALLET_2 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y | jq '.'
-sleep 2
-$CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
+delegator_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
+validator_bond_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
+submit_tx "tx staking delegate $VALOPER_1 $delegation$DENOM --from $WALLET_2 -o json --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+delegator_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
+
+echo $shares_diff
+shares_diff=$(($delegator_shares_1-$delegator_shares_2))
+if [[ $shares_diff -ne $delegation ]]; then
+    echo "Delegation unsuccessful."
+    return 1
+fi
+
 $CHAIN_BINARY tx staking validator-bond $VALOPER_1 --from $WALLET_2 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y --fees $BASE_FEES$DENOM | jq '.'
-$CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
+validator_bond_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
+bond_shares_diff=$(($validator_bond_shares_1-$validator_bond_shares_2))
+echo $bond_shares_diff
+if [[ $shares_diff -ne $delgation  ]]; then
+    echo "Validator bond unsuccessful."
+    return 1
+fi
