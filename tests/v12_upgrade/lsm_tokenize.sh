8bb84b50ffff58c8c15c2071b661cbe0791499d2
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-08 10:36:36 -0400

    tokenize test

--- tests/v12_upgrade/lsm_tokenize.sh
+++ tests/v12_upgrade/lsm_tokenize.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+delegation=100000000
+tokenize=50000000
+delegator_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
+liquid_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_liquid_shares')
+
 echo "Delegating with $WALLET_3..."
-$CHAIN_BINARY tx staking delegate $VALOPER_1 100000000$DENOM --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y | jq '.'
-sleep 2
-$CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
+submit_tx "tx staking delegate $VALOPER_1 $delegation$DENOM --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+
+delegator_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
+shares_diff=$((${delegator_shares_2%.*}-${delegator_shares_1%.*})) # remove decimal portion
+echo "Delegator shares difference: $shares_diff"
+if [[ $shares_diff -ne $delegation ]]; then
+    echo "Delegation unsuccessful."
+    exit 1
+fi
 
 echo "Tokenizing shares with $WALLET_3..."
-$CHAIN_BINARY tx staking tokenize-share $VALOPER_1 50000000$DENOM $WALLET_3 --from $WALLET_3 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y | jq '.'
-sleep 2
-$CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq '.'
-$CHAIN_BINARY q bank balances $WALLET_3 --home $HOME_1 -o json | jq '.'
+sumbit_tx "tx staking tokenize-share $VALOPER_1 $tokenize$DENOM $WALLET_3 --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+
+liquid_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_liquid_shares')
+$liquid_shares_diff=$((${liquid_shares_2%.*}-${liquid_shares_1%.*})) # remove decimal portion
+echo "Liquid shares difference: $liquid_shares_diff"
+if [[ $liquid_shares_diff -ne $delegation ]]; then
+    echo "Tokenize unsuccessful: unexpected liquid shares amount"
+    exit 1
+fi
+
+lsm_denom=$VALOPER_1/1
+liquid_balance=$($CHAIN_BINARY q bank balances $WALLET_3 --home $HOME_1 -o json | jq -r --arg DENOM "$lsm_denom" '.balances[] | select(.denom==$DENOM).amount')
+echo "Liquid balance: $liquid_balance"
+if [[ $liquid_balance -ne $tokenize ]]; then
+    echo "Tokenize unsusccessful: unexpected liquid token balance"
+    exit 1
+fi
--- tests/v12_upgrade/lsm_validator_bond.sh
+++ tests/v12_upgrade/lsm_validator_bond.sh
@@ -2,25 +2,26 @@
 
 source tests/process_tx.sh
 delegation=100000000
-
-echo "Delegating with $WALLET_2..."
 delegator_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 validator_bond_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
-submit_tx "tx staking delegate $VALOPER_1 $delegation$DENOM --from $WALLET_2 -o json --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-delegator_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 
+echo "Delegating with $WALLET_2..."
+submit_tx "tx staking delegate $VALOPER_1 $delegation$DENOM --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+
+delegator_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 shares_diff=$((${delegator_shares_2%.*}-${delegator_shares_1%.*})) # remove decimal portion
-echo $shares_diff
+echo "Delegator shares difference: $shares_diff"
 if [[ $shares_diff -ne $delegation ]]; then
     echo "Delegation unsuccessful."
     exit 1
 fi
 
 echo "Validator bond with $WALLET_2..."
-$CHAIN_BINARY tx staking validator-bond $VALOPER_1 --from $WALLET_2 -o json --home $HOME_1 --gas auto --gas-adjustment 1.2 -y --fees $BASE_FEES$DENOM | jq '.'
+submit_tx "tx staking validator-bond $VALOPER_1 --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+
 validator_bond_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
 bond_shares_diff=$((${validator_bond_shares_2%.*}-${validator_bond_shares_1%.*})) # remove decimal portion
-echo $bond_shares_diff
+echo "Bond shares difference: $bond_shares_diff"
 if [[ $shares_diff -ne $delegation  ]]; then
     echo "Validator bond unsuccessful."
     exit 1
