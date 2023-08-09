f95a54fb750b32a97c565635d76bc44200be5b5a
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-08 20:46:40 -0400

    redeem tests

--- tests/v12_upgrade/lsm_redeem.sh
+++ tests/v12_upgrade/lsm_redeem.sh
@@ -30,18 +30,71 @@ echo "Redeeming tokens from $WALLET_3..."
 submit_tx "tx staking redeem-tokens 20000000$tokenized_denom --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.'
 echo "Redeeming tokens from $WALLET_4..."
-submit_tx "tx staking redeem-tokens 20000000$tokenized_denom --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
+submit_tx "tx staking redeem-tokens $bank_send_amount$tokenized_denom --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.'
 echo "Transferring $WALLET_5 IBC tokens to LSM chain with..."
 echo "IBC denom: $ibc_denom"
 echo "Sending tokens from $WALLET_5 to $CHAIN_ID for redeem operation..."
-submit_tx "tx ibc-transfer transfer transfer channel-1 $WALLET_5 10000000$ibc_denom --from $STRIDE_WALLET_5 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $CONSUMER_FEES$STRIDE_DENOM -y" $STRIDE_CHAIN_BINARY $STRIDE_HOME_1
+submit_tx "tx ibc-transfer transfer transfer channel-1 $WALLET_5 $ibc_transfer_amount$ibc_denom --from $STRIDE_WALLET_5 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $CONSUMER_FEES$STRIDE_DENOM -y" $STRIDE_CHAIN_BINARY $STRIDE_HOME_1
 echo "Waiting for IBC tokens to reach $CHAIN_ID..."
 sleep 10
 echo "Redeeming tokens from $WALLET_5..."
-submit_tx "tx staking redeem-tokens 10000000$tokenized_denom --from $WALLET_5 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
-$CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.'
+submit_tx "tx staking redeem-tokens $ibc_transfer_amount$tokenized_denom --from $WALLET_5 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
 
-$CHAIN_BINARY q staking delegations $WALLET_3 --home $HOME_1 -o json | jq -r '.'
-$CHAIN_BINARY q staking delegations $WALLET_4 --home $HOME_1 -o json | jq -r '.'
-$CHAIN_BINARY q staking delegations $WALLET_5 --home $HOME_1 -o json | jq -r '.'
+wallet_3_delegations_2=$($CHAIN_BINARY q staking delegations $WALLET_3 --home $HOME_1 -o json | jq -r --arg ADDRESS "$VALOPER_1" '.delegation_responses[] | select(.delegation.validator_address==$ADDRESS).delegation.shares')
+wallet_3_delegations_diff=$((${wallet_3_delegations_2%.*}-${wallet_3_delegations_1%.*}))
+wallet_4_delegations=$($CHAIN_BINARY q staking delegations $WALLET_4 --home $HOME_1 -o json | jq -r --arg ADDRESS "$VALOPER_1" '.delegation_responses[] | select(.delegation.validator_address==$ADDRESS).delegation.shares')
+wallet_5_delegations=$($CHAIN_BINARY q staking delegations $WALLET_5 --home $HOME_1 -o json | jq -r --arg ADDRESS "$VALOPER_1" '.delegation_responses[] | select(.delegation.validator_address==$ADDRESS).delegation.shares')
+wallet_3_delegation_balance=$($CHAIN_BINARY q staking delegations $WALLET_3 --home $HOME_1 -o json | jq -r --arg ADDRESS "$VALOPER_1" '.delegation_responses[] | select(.delegation.validator_address==$ADDRESS).balance.amount')
+wallet_4_delegation_balance=$($CHAIN_BINARY q staking delegations $WALLET_4 --home $HOME_1 -o json | jq -r --arg ADDRESS "$VALOPER_1" '.delegation_responses[] | select(.delegation.validator_address==$ADDRESS).balance.amount')
+wallet_5_delegation_balance=$($CHAIN_BINARY q staking delegations $WALLET_5 --home $HOME_1 -o json | jq -r --arg ADDRESS "$VALOPER_1" '.delegation_responses[] | select(.delegation.validator_address==$ADDRESS).balance.amount')
+validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
+validator_liquid_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_liquid_shares')
+
+echo "Wallet 3 delegation shares increase: $wallet_3_delegations_diff"
+if [[ $wallet_3_delegations_diff -ne 70000000 ]]; then
+    echo "Redeem unsuccessful: unexpected delegation shares for wallet 3"
+    exit 1
+fi
+
+echo "Wallet 3 delegation balance: $wallet_3_delegation_balance"
+if [[ $wallet_3_delegation_balance -ne 70000000 ]]; then
+    echo "Redeem unsuccessful: unexpected delegation balance for wallet 3"
+    exit 1
+fi
+
+echo "Wallet 4 delegation shares: ${wallet_4_delegations%.*}"
+if [[ ${wallet_4_delegations_diff%.*} -ne $bank_send_amount ]]; then
+    echo "Redeem unsuccessful: unexpected delegation shares for wallet 4"
+    exit 1
+fi
+
+echo "Wallet 4 delegation balance: $wallet_4_delegation_balance"
+if [[ $wallet_4_delegation_balance -ne $bank_send_amount ]]; then
+    echo "Redeem unsuccessful: unexpected delegation balance for wallet 4"
+    exit 1
+fi
+
+echo "Wallet 5 delegation shares: ${wallet_5_delegations%.*}"
+if [[ ${wallet_5_delegations_diff%.*} -ne $ibc_transfer_amount ]]; then
+    echo "Redeem unsuccessful: unexpected delegation shares for wallet 5"
+    exit 1
+fi
+
+echo "Wallet 5 delegation balance: $wallet_5_delegation_balance"
+if [[ $wallet_5_delegation_balance -ne $ibc_transfer_amount ]]; then
+    echo "Redeem unsuccessful: unexpected delegation balance for wallet 5"
+    exit 1
+fi
+
+echo "Validator bond shares difference: ${validator_bond_shares%.*}"
+if [[ ${validator_bond_shares%.*} -ne 100000000  ]]; then
+    echo "Redeem unsuccessful: unexpected validator bond shares amount"
+    exit 1
+fi
+
+echo "Validator bond shares difference: ${validator_liquid_shares%.*}"
+if [[ ${validator_liquid_shares%.*} -ne 0  ]]; then
+    echo "Redeem unsuccessful: unexpected validator liquid shares amount"
+    exit 1
+fi
