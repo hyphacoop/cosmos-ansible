412761a37e6e33f22deb0c5128154cf1b38fd942
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-09 08:52:42 -0400

    query delegations and balances

--- tests/v12_upgrade/lsm_failures.sh
+++ tests/v12_upgrade/lsm_failures.sh
@@ -5,10 +5,14 @@ delegation=100000000
 delegator_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 validator_bond_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
 
+$CHAIN_BINARY q staking delegations $WALLET_4 --home $HOME_1 -o json | jq -r '.'
+$CHAIN_BINARY q bank balances $WALLET_4 --home $HOME_1 -o json | jq -r '.'
+
 echo "Attempt to tokenize with $WALLET_4..."
 submit_bad_tx "tx staking tokenize-share $VALOPER_1 20000000$DENOM $WALLET_4 --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
-
+$CHAIN_BINARY q staking delegations $WALLET_4 --home $HOME_1 -o json | jq -r '.'
+$CHAIN_BINARY q bank balances $WALLET_4 --home $HOME_1 -o json | jq -r '.'
 # echo "Validator bond with $WALLET_2..."
 # submit_tx "tx staking validator-bond $VALOPER_1 --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
 
