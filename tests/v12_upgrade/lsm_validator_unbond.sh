7d622f28bc97c6e988f5d8a1d3b7fe14f3f4a2f5
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-08 21:45:04 -0400

    unbond oip

--- tests/v12_upgrade/lsm_validator_unbond.sh
+++ tests/v12_upgrade/lsm_validator_unbond.sh
@@ -3,5 +3,5 @@
 source tests/process_tx.sh
 
 echo "Validator unbond from $WALLET_2..."
-submit_tx "tx staking unbond-validator --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+submit_tx "tx staking unbond $VALOPER_1 100000000$DENOM --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
 $CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json
