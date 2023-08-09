fc547cf61e659564ac381555b85fb1e1eccb5825
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-08 22:21:27 -0400

    tokenize cmd

--- tests/v12_upgrade/lsm_failures.sh
+++ tests/v12_upgrade/lsm_failures.sh
@@ -6,7 +6,7 @@ delegator_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1
 validator_bond_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
 
 echo "Attempt to tokenize with $WALLET_4..."
-submit_bad_tx "tx staking tokenize-share $VALOPER_1 20000000$DENOM --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+submit_bad_tx "tx staking tokenize-share $VALOPER_1 20000000$DENOM $WALLET_4 --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 
 # echo "Validator bond with $WALLET_2..."
