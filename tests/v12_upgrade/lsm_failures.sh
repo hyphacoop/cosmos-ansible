084d5e73df8199c9c7971d724d1efa76e3dfb885
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-09 13:24:35 -0400

    print validator

--- tests/v12_upgrade/lsm_failures.sh
+++ tests/v12_upgrade/lsm_failures.sh
@@ -4,9 +4,11 @@ source tests/process_tx.sh
 
 validator_cap_param=$($CHAIN_BINARY q staking params --home $HOME_1 -o json | jq -r '.validator_liquid_staking_cap')
 global_cap_param=$($CHAIN_BINARY q staking params --home $HOME_1 -o json | jq -r '.global_liquid_staking_cap')
+$CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.'
 
 echo "Delegating with WALLET_5..."
 submit_tx "tx staking delegate $VALOPER_1 120000000$DENOM --from $WALLET_5 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+submit_tx "tx staking delegate $VALOPER_2 120000000$DENOM --from $WALLET_5 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 echo "Failure case 1: Attempt to tokenize with WALLET_4 (no validator bond)..."
 submit_bad_tx "tx staking tokenize-share $VALOPER_1 20000000$DENOM $WALLET_4 --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
@@ -24,10 +26,10 @@ submit_bad_tx "tx staking tokenize-share $VALOPER_1 20000000$DENOM $WALLET_4 --f
 bonded_tokens=$($CHAIN_BINARY q staking pool --home $HOME_1 -o json | jq -r '.bonded_tokens')
 validator_delegations=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 global_staked=$($CHAIN_BINARY q staking total-liquid-staked --home $HOME_1 -o json | jq -r '.')
-cap_per_validator=$(echo "$bonded_tokens*$validator_cap_param" | bc)
+validator_cap=$(echo "$validator_delegations*$validator_cap_param" | bc)
 global_cap=$(echo "$bonded_tokens*$global_cap_param" | bc)
 echo "Validator_delegations: ${validator_delegations%.*}"
-echo "Validator shares cap: ${cap_per_validator%.*}"
+echo "Validator shares cap: ${validator_cap%.*}"
 echo "Global shares cap: ${global_cap%.*}"
 echo "Global staked: $global_staked"
 # echo "Global total: 
@@ -35,3 +37,10 @@ echo "Failure case 3: Attempt to tokenize with WALLET_5, breaching the validator
 submit_bad_tx "tx staking tokenize-share $VALOPER_1 120000000$DENOM $WALLET_5 --from $WALLET_5 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 echo "Failure case 4: Attempt to tokenize with $WALLET_5, breaching the global liquid staking cap..."
+submit_tx "tx staking validator-bond $VALOPER_2 --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
+echo "Validator bond shares: ${validator_bond_shares%.*}"
+if [[ ${validator_bond_shares%.*} -ne 20000000  ]]; then
+    echo "Validator bond unsuccessful."
+    exit 1
+fi
