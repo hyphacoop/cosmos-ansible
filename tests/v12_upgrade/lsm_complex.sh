02c0b01ff3f79bc8891c26531b68b74f37ba6ede
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-10 08:57:02 -0400

    unbond from tokenizing account

--- tests/v12_upgrade/lsm_complex.sh
+++ tests/v12_upgrade/lsm_complex.sh
@@ -31,55 +31,37 @@ if [[ ${validator_bond_shares%.*} -ne $delegation  ]]; then
 fi
 
 # ** SCENARIO 1 **
-
+echo "** SCENARIO 1: delegate - tokenize - slash - redeem **"
 echo "Delegating with tokenizing_account..."
 submit_tx "tx staking delegate $VALOPER_2 $tokenize$DENOM --from liquid_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-$CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq '.'
-
 delegation_balance_pre_tokenize=$($CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
+slash_fraction=$($CHAIN_BINARY q slashing params --home $HOME_1 -o json | jq -r '.slash_fraction_downtime')
+expected_balance=$(echo "$delegation_balance_pre_tokenize-($delegation_balance_pre_tokenize*$slash_fraction)" | bc)
 echo "Tokenizing with tokenizing account..."
 submit_tx "tx staking tokenize-share $VALOPER_2 $tokenize$DENOM $liquid_address --from liquid_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-$CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq '.'
 echo "Slashing validator 2..."
 tests/major_fresh_upgrade/jail_validator.sh $PROVIDER_SERVICE_2 $VALOPER_2
 echo "Redeeming with tokenizing account..."
 submit_tx "tx staking redeem-tokens $tokenize$tokenized_denom --from liquid_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-$CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq '.'
 delegation_balance_post_redeem=$($CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
-slash_fraction=$($CHAIN_BINARY q slashing params --home $HOME_1 -o json | jq -r '.slash_fraction_downtime')
-expected_balance=$(echo "$delegation_balance_pre_tokenize-($delegation_balance_pre_tokenize*$slash_fraction)" | bc)
 echo "New balance: $delegation_balance_post_redeem"
 echo "Expected new balance: $expected_balance"
-echo "Unjailing validator 2..."
-tests/major_fresh_upgrade/unjail_validator.sh $PROVIDER_SERVICE_2 $VAL2_RPC_PORT $WALLET_2 $VALOPER_2
-$CHAIN_BINARY q staking validator $VALOPER_2 --home $HOME_1 -o json | jq '.'
-
 if [[ $delegation_balance_post_redeem -ne ${expected_balance%.*} ]]; then
     echo "Complex scenario 1 failed: Unexpected post-redeem balance ($delegation_balance_post_redeem)"
     exit 1
 fi
 
-# delegator_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
-# shares_diff=$((${delegator_shares_2%.*}-${delegator_shares_1%.*})) # remove decimal portion
-# echo "Delegator shares difference: $shares_diff"
-# if [[ $shares_diff -ne $delegation ]]; then
-#     echo "Delegation unsuccessful."
-#     exit 1
-# fi
+echo "Unjailing validator 2..."
+tests/major_fresh_upgrade/unjail_validator.sh $PROVIDER_SERVICE_2 $VAL2_RPC_PORT $WALLET_2 $VALOPER_2
+$CHAIN_BINARY q staking validator $VALOPER_2 --home $HOME_1 -o json | jq '.'
 
-# echo "Tokenizing shares with $WALLET_3..."
-# submit_tx "tx staking tokenize-share $VALOPER_1 $tokenize$DENOM $WALLET_3 --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+echo "Unbonding from tokenizing account..."
+sumbit_tx "tx staking unbond $VALOPER_2 $tokenize$DENOM --from liquid_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+$CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq '.'
 
-# liquid_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_liquid_shares')
-# echo "Liquid shares: ${liquid_shares%.*}"
-# if [[ ${liquid_shares%.*} -ne $tokenize ]]; then
-#     echo "Tokenize unsuccessful: unexpected liquid shares amount"
-#     exit 1
-# fi
-
-# liquid_balance=$($CHAIN_BINARY q bank balances $WALLET_3 --home $HOME_1 -o json | jq -r --arg DENOM "$tokenized_denom" '.balances[] | select(.denom==$DENOM).amount')
-# echo "Liquid balance: ${liquid_balance%.*}"
-# if [[ ${liquid_balance%.*} -ne $tokenize ]]; then
-#     echo "Tokenize unsuccessful: unexpected liquid token balance"
-#     exit 1
-# fi
+# ** SCENARIO 2 **
+echo "** SCENARIO 2: delegate - slash - tokenize - redeem **"
+echo "Delegating with tokenizing_account..."
+submit_tx "tx staking delegate $VALOPER_2 $tokenize$DENOM --from liquid_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+$CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq '.'
+delegation_balance_pre_tokenize=$($CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
