5c72a752d9fdf7cc3f2528ebe8318da9bec8f056
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-10 16:49:44 -0400

    delegate to validator with bond shares

--- tests/v12_upgrade/ica_delegate.sh
+++ tests/v12_upgrade/ica_delegate.sh
@@ -9,3 +9,13 @@ echo "Sending tx..."
 $STRIDE_CHAIN_BINARY tx interchain-accounts controller send-tx connection-0 delegate_packet.json --from $MONIKER_1 --home $STRIDE_HOME_1 --chain-id $STRIDE_CHAIN_ID --gas auto --fees $BASE_FEES$STRIDE_DENOM --gas-adjustment 1.2 -y -o json | jq '.'
 sleep 10
 $CHAIN_BINARY q staking validator $VALOPER_1 -o json --home $HOME_1 | jq '.'
+
+$CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.'
+jq -r --arg ADDRESS "$ICA_ADDRESS" '.delegator_address = $ADDRESS' tests/v12_upgrade/msg-delegate.json > delegate-2.json
+message=$(jq -r --arg ADDRESS "$VALOPER_2" '.validator_address = $ADDRESS' delegate-2.json)
+echo "Generating packet JSON..."
+$STRIDE_CHAIN_BINARY tx interchain-accounts host generate-packet-data "$message" > delegate_packet.json
+echo "Sending tx..."
+$STRIDE_CHAIN_BINARY tx interchain-accounts controller send-tx connection-0 delegate_packet.json --from $MONIKER_1 --home $STRIDE_HOME_1 --chain-id $STRIDE_CHAIN_ID --gas auto --fees $BASE_FEES$STRIDE_DENOM --gas-adjustment 1.2 -y -o json | jq '.'
+sleep 10
+$CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.'
