28d342cd40eed18696bcfae025fdfdae4dac41d2
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-09 23:39:59 -0400

    tokenize from new accounts

--- tests/v12_upgrade/lsm_complex.sh
+++ tests/v12_upgrade/lsm_complex.sh
@@ -7,13 +7,24 @@ source tests/process_tx.sh
 
 delegation=10000000
 tokenize=50000000
-tokenized_denom=$VALOPER_2/1
+# tokenized_denom=$VALOPER_2/1
 delegator_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 
-echo "Delegating with WALLET_4..."
-submit_tx "tx staking delegate $VALOPER_2 $delegation$DENOM --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-echo "Validator bond with WALLET 4..."
-submit_tx "tx staking validator-bond $VALOPER_2 --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+$CHAIN_BINARY keys add bonding_account --home $HOME_1
+$CHAIN_BINARY keys add liquid_account --home $HOME_1
+bonding_address=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="bonding_account").address')
+liquid_address=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="liquid_account").address')
+
+echo "Funding bonding and tokenizing accounts..."
+submit_tx "tx bank send $WALLET_1 $bonding_address 100000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+submit_tx "tx bank send $WALLET_1 $liquid_address 100000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+
+# ** SCENARIO 1 **
+
+echo "Delegating with bonding_account..."
+submit_tx "tx staking delegate $VALOPER_2 $delegation$DENOM --from bonding_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+echo "Validator bond with bonding_account..."
+submit_tx "tx staking validator-bond $VALOPER_2 --from bonding_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
 
 validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_2 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
 echo "Validator 2 bond shares: $validator_bond_shares"
@@ -22,7 +33,15 @@ if [[ $validator_bond_shares -ne $delegation  ]]; then
     exit 1
 fi
 
-$CHAIN_BINARY q bank balances $WALLET_4 -o json | jq '.'
+echo "Delegating with tokenizing_account..."
+submit_tx "tx staking delegate $VALOPER_2 $delegation$DENOM --from liquid_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+echo "Tokenizing with tokenizing account..."
+submit_tx "tx staking tokenize-share $VALOPER_2 $tokenize$DENOM $liquid_address --from liquid_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+
+$CHAIN_BINARY q bank balances $liquid_address -o json --home $HOME_1 | jq '.'
+
+# echo "Redeeming with tokenizing account..."
+# submit_tx "tx staking redeem-tokens $tokenize$tokenized_denom --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
 
 # delegator_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 # shares_diff=$((${delegator_shares_2%.*}-${delegator_shares_1%.*})) # remove decimal portion
