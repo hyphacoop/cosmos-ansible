ee92f443febd1ac666936b823cf3b2ee43a6cc21
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-10 11:57:17 -0400

    unbond after accounting tests

--- tests/v12_upgrade/lsm_accounting.sh
+++ tests/v12_upgrade/lsm_accounting.sh
@@ -49,9 +49,13 @@ val_delta=$(($val_liquid_2-$val_liquid_1))
 total_delta=$(($total_liquid_2-$total_liquid_1))
 if [[ $val_delta -ne $tokenize ]]; then
     echo "Accounting failure: unexpected validator liquid shares increase ($val_delta)"
+else
+    echo "Accounting success: expected validator liquid shares increase ($val_delta)"
 fi
 if [[ $total_delta -ne $tokenize ]]; then
     echo "Accounting failure: unexpected global liquid tokens increase ($total_delta)"
+else
+    echo "Accounting success: expected global liquid tokens increase ($total_delta)"
 fi
 
 echo "Redeeming with tokenizing account..."
@@ -63,11 +67,19 @@ val_delta=$(($val_liquid_2-$val_liquid_3))
 total_delta=$(($total_liquid_2-$total_liquid_3))
 if [[ $val_delta -ne $tokenize ]]; then
     echo "Accounting failure: unexpected validator liquid shares decrease ($val_delta)"
+else
+    echo "Accounting success: expected validator liquid shares decrease ($val_delta)"
 fi
 if [[ $total_delta -ne $tokenize ]]; then
     echo "Accounting failure: unexpected global liquid tokens decrease ($total_delta)"
+else
+    echo "Accounting success: expected global liquid tokens decrease ($total_delta)"
 fi
 
-delegation_balance_post_redeem=$($CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
-$CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.'
-$CHAIN_BINARY q staking total-liquid-staked -o json --home $HOME_1 | jq '.'
+echo "Unbonding from tokenizing account..."
+delegation_balance=$($CHAIN_BINARY q staking delegations $liquid_address --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
+submit_tx "tx staking unbond $VALOPER_2 ${delegation_balance%.*}$DENOM --from $liquid_address -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+echo "Unbonding from bonding account..."
+delegation_balance=$($CHAIN_BINARY q staking delegations $bonding_address --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
+submit_tx "tx staking unbond $VALOPER_2 ${delegation_balance%.*}$DENOM --from $bonding_address -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
+$CHAIN_BINARY q staking validator $VALOPER_2 --home $HOME_1 -o json | jq '.'
