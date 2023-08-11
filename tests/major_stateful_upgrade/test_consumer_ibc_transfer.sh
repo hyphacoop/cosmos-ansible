a16dc72a9f6041cb596118bb72e5b45d72045b95
ASoTNetworks <dev@aseriesoftubez.com>
2023-08-11 15:08:35 -0400

    allow to set wallet

--- tests/major_stateful_upgrade/test_consumer_ibc_transfer.sh
+++ tests/major_stateful_upgrade/test_consumer_ibc_transfer.sh
@@ -5,12 +5,25 @@ echo "Running with $CONSUMER_CHAIN_BINARY."
 
 PROVIDER_CHANNEL=$1
 EXPECTED_DENOMS=$2
+PROVIDER_WALLET=$3
+CONSUMER_WALLET=$4
+
+if [ ! $PROVIDER_WALLET ]
+then
+    PROVIDER_WALLET=$WALLET_1
+fi
+
+if [ ! $CONSUMER_WALLET ]
+then
+    CONSUMER_WALLET=$WALLET_1
+fi
+
 # Transfer provider token to consumer chain
 echo "Sending $DENOM to $CONSUMER_CHAIN_ID..."
-$CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer $PROVIDER_CHANNEL $WALLET_1 1000$DENOM --from $WALLET_1 --keyring-backend test --gas 500000 --fees 5000$DENOM -b block -y --chain-id $CHAIN_ID
+$CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer $PROVIDER_CHANNEL $CONSUMER_WALLET 1000$DENOM --from $PROVIDER_WALLET --keyring-backend test --gas 500000 --fees 5000$DENOM -b block -y --chain-id $CHAIN_ID
 echo "Waiting for the transfer to reach the consumer chain..."
 sleep 30
-DENOM_AMOUNT=$($CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $WALLET_1 -o json | jq -r '.balances | length')
+DENOM_AMOUNT=$($CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $CONSUMER_WALLET -o json | jq -r '.balances | length')
 if [ $DENOM_AMOUNT -lt 2 ]; then
     echo "Only one denom found in consumer wallet."
     exit 1
@@ -19,11 +32,11 @@ echo "Found at least two denoms in the consumer wallet."
 
 # Transfer consumer token to provider chain
 echo "Sending $CONSUMER_DENOM to $CHAIN_ID..."
-$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $WALLET_1 1000$CONSUMER_DENOM --from $WALLET_1 --keyring-backend test -y -b sync
+$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $PROVIDER_WALLET 1000$CONSUMER_DENOM --from $CONSUMER_WALLET --keyring-backend test -y -b sync
 echo "Waiting for the transfer to reach the provider chain..."
 sleep 60
-$CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json
-DENOM_AMOUNT=$($CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json | jq -r '.balances | length')
+$CHAIN_BINARY --home $HOME_1 q bank balances $PROVIDER_WALLET -o json
+DENOM_AMOUNT=$($CHAIN_BINARY --home $HOME_1 q bank balances $PROVIDER_WALLET -o json | jq -r '.balances | length')
 if [ $DENOM_AMOUNT -lt $EXPECTED_DENOMS ]; then
     echo "Found less than expected denoms in provider wallet."
     exit 1
