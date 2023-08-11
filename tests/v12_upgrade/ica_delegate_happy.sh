1d599bf6bc9e51a4f25794d9726b9228398bb1e8
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-11 09:11:40 -0400

    wait for ICA delegation

--- tests/v12_upgrade/ica_delegate_happy.sh
+++ tests/v12_upgrade/ica_delegate_happy.sh
@@ -4,7 +4,8 @@
 source tests/process_tx.sh
 
 echo "Sending tx staking delegate to host chain..."
-$CHAIN_BINARY q staking validator $VALOPER_1 -o json --home $HOME_1 | jq '.'
+$CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.'
+$CHAIN_BINARY q bank balances $$ICA_ADDRESS -o json --home $HOME_1 | jq '.'
 jq -r --arg ADDRESS "$ICA_ADDRESS" '.delegator_address = $ADDRESS' tests/v12_upgrade/msg-delegate.json > delegate-happy.json
 message=$(jq -r --arg ADDRESS "$VALOPER_2" '.validator_address = $ADDRESS' delegate-happy.json)
 echo "Generating packet JSON..."
@@ -12,7 +13,8 @@ $STRIDE_CHAIN_BINARY tx interchain-accounts host generate-packet-data "$message"
 echo "Sending tx..."
 submit_ibc_tx "tx interchain-accounts controller send-tx connection-0 delegate_packet.json --from $MONIKER_1 --chain-id $STRIDE_CHAIN_ID --gas auto --fees $BASE_FEES$STRIDE_DENOM --gas-adjustment 1.2 -y -o json" $STRIDE_CHAIN_BINARY $STRIDE_HOME_1
 # $STRIDE_CHAIN_BINARY tx interchain-accounts controller send-tx connection-0 delegate_packet.json --from $MONIKER_1 --home $STRIDE_HOME_1 --chain-id $STRIDE_CHAIN_ID --gas auto --fees $BASE_FEES$STRIDE_DENOM --gas-adjustment 1.2 -y -o json | jq '.'
-# sleep 10
+echo "Waiting for delegation to go on-chain..."
+sleep 10
 $CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.'
 $CHAIN_BINARY q bank balances $$ICA_ADDRESS -o json --home $HOME_1 | jq '.'
 
