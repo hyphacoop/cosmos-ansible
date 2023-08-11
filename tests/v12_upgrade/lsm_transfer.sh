306318b63aad6d62267d97309a850c407ddc0553
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-11 14:40:58 -0400

    lsm transfer

--- tests/v12_upgrade/lsm_transfer.sh
+++ tests/v12_upgrade/lsm_transfer.sh
@@ -5,13 +5,27 @@ source tests/process_tx.sh
 
 tokenized_denom="$VALOPER_1/1"
 
-echo "Transferring token ownership record..."
+
 record_id=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.id')
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
+
+echo "Transferring token ownership record to WALLET_4..."
 submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_4 --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
+if [[ $owner -eq $WALLET_4]]; then
+    echo "Token ownership transfer succeeded."
+else
+    echo "Token ownership transfer failed."
+fi
+
+echo "Transferring token ownership record back to WALLET_3..."
 submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_3 --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
+if [[ $owner -eq $WALLET_3]]; then
+    echo "Token ownership transfer succeeded."
+else
+    echo "Token ownership transfer failed."
+fi
