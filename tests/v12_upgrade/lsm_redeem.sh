95c3ae55d0c13d7b43dd8dfaac3253d44fdb156e
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-11 13:39:47 -0400

    unbond after happy path

--- tests/v12_upgrade/lsm_redeem.sh
+++ tests/v12_upgrade/lsm_redeem.sh
@@ -99,30 +99,29 @@ fi
 #     exit 1
 # fi
 
+echo "Validator unbond from WALLET_2..."
+submit_tx "tx staking unbond $VALOPER_1 100000000$DENOM --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
+echo "Validator bond shares: ${validator_bond_shares%.*}"
+if [[ ${validator_bond_shares%.*} -ne 0  ]]; then
+    echo "Unbond unsuccessful: unexpected validator bond shares amount"
+    exit 1
+fi
 
-# echo "Validator unbond from WALLET_2..."
-# submit_tx "tx staking unbond $VALOPER_1 100000000$DENOM --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
-# validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
-# echo "Validator bond shares: ${validator_bond_shares%.*}"
-# if [[ ${validator_bond_shares%.*} -ne 0  ]]; then
-#     echo "Unbond unsuccessful: unexpected validator bond shares amount"
-#     exit 1
-# fi
+echo "Validator unbond from WALLET_3..."
+submit_tx "tx staking unbond $VALOPER_1 80000000$DENOM --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
+echo "Validator bond shares: ${validator_bond_shares%.*}"
+if [[ ${validator_bond_shares%.*} -ne 0  ]]; then
+    echo "Unbond unsuccessful: unexpected validator bond shares amount"
+    exit 1
+fi
 
-# echo "Validator unbond from WALLET_3..."
-# submit_tx "tx staking unbond $VALOPER_1 80000000$DENOM --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
-# validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
-# echo "Validator bond shares: ${validator_bond_shares%.*}"
-# if [[ ${validator_bond_shares%.*} -ne 0  ]]; then
-#     echo "Unbond unsuccessful: unexpected validator bond shares amount"
-#     exit 1
-# fi
-
-# echo "Validator unbond from WALLET_4..."
-# submit_tx "tx staking unbond $VALOPER_1 $bank_send_amount$DENOM --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
-# validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
-# echo "Validator bond shares: ${validator_bond_shares%.*}"
-# if [[ ${validator_bond_shares%.*} -ne 0  ]]; then
-#     echo "Unbond unsuccessful: unexpected validator bond shares amount"
-#     exit 1
-# fi
+echo "Validator unbond from WALLET_4..."
+submit_tx "tx staking unbond $VALOPER_1 $bank_send_amount$DENOM --from $WALLET_4 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
+echo "Validator bond shares: ${validator_bond_shares%.*}"
+if [[ ${validator_bond_shares%.*} -ne 0  ]]; then
+    echo "Unbond unsuccessful: unexpected validator bond shares amount"
+    exit 1
+fi
