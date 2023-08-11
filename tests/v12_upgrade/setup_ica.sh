267eb1b85262a14d0c25a232729e27db9acd2c35
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-11 16:09:54 -0400

    increase ibc tx wait time

--- tests/v12_upgrade/setup_ica.sh
+++ tests/v12_upgrade/setup_ica.sh
@@ -2,9 +2,8 @@
 source tests/process_tx.sh
 
 echo "Registering ICA account..."
-submit_ibc_tx "tx interchain-accounts controller register connection-0 --from $STRIDE_WALLET_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$STRIDE_DENOM -y --chain-id $STRIDE_CHAIN_ID -o json" $STRIDE_CHAIN_BINARY $STRIDE_HOME_1
-# $STRIDE_CHAIN_BINARY tx interchain-accounts controller register connection-0 --from $STRIDE_WALLET_1 --gas auto --gas-adjustment 1.2 --fees $BASE_FEES$STRIDE_DENOM -y --home $STRIDE_HOME_1 --chain-id $STRIDE_CHAIN_ID -o json | jq '.'
-# sleep 10
+submit_ibc_tx "tx interchain-accounts controller register connection-0 --from $STRIDE_WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$STRIDE_DENOM -y --chain-id $STRIDE_CHAIN_ID -o json" $STRIDE_CHAIN_BINARY $STRIDE_HOME_1
+
 ica_address=$($STRIDE_CHAIN_BINARY q interchain-accounts controller interchain-account $STRIDE_WALLET_1 connection-0 --home $STRIDE_HOME_1 -o json | jq -r '.address')
 echo "ICA address: $ica_address"
 echo "Funding ICA address in chain $CHAIN_ID..."
