76ae78942e03d1833210beb3252104a00bc5cedd
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-11 14:47:26 -0400

    string compare syntax

--- tests/v12_upgrade/lsm_transfer.sh
+++ tests/v12_upgrade/lsm_transfer.sh
@@ -5,7 +5,6 @@ source tests/process_tx.sh
 
 tokenized_denom="$VALOPER_1/1"
 
-
 record_id=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.id')
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
@@ -14,7 +13,7 @@ echo "Transferring token ownership record to WALLET_4..."
 submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_4 --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
-if [[ $owner -eq $WALLET_4]]; then
+if [[ "$owner" == "$WALLET_4" ]]; then
     echo "Token ownership transfer succeeded."
 else
     echo "Token ownership transfer failed."
@@ -24,7 +23,7 @@ echo "Transferring token ownership record back to WALLET_3..."
 submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_3 --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
-if [[ $owner -eq $WALLET_3]]; then
+if [[ "$owner" == "$WALLET_3" ]]; then
     echo "Token ownership transfer succeeded."
 else
     echo "Token ownership transfer failed."
