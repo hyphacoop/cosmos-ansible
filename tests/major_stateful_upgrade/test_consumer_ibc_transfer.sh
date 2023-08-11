190386d17dd5ef265314ab26ff3c154b625b0723
ASoTNetworks <dev@aseriesoftubez.com>
2023-08-11 15:32:31 -0400

    add fees on comsumer ibc

--- tests/major_stateful_upgrade/test_consumer_ibc_transfer.sh
+++ tests/major_stateful_upgrade/test_consumer_ibc_transfer.sh
@@ -19,10 +19,14 @@ then
 fi
 
 # Transfer provider token to consumer chain
+echo "Current $CONSUMER_CHAIN_ID bank blances: $CONSUMER_WALLET"
+$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $CONSUMER_WALLET
 echo "Sending $DENOM to $CONSUMER_CHAIN_ID..."
 $CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer $PROVIDER_CHANNEL $CONSUMER_WALLET 1000$DENOM --from $PROVIDER_WALLET --keyring-backend test --gas 500000 --fees 5000$DENOM -b block -y --chain-id $CHAIN_ID
 echo "Waiting for the transfer to reach the consumer chain..."
-sleep 30
+sleep 60
+echo "$CONSUMER_CHAIN_ID bank blances after sending: $CONSUMER_WALLET"
+$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $CONSUMER_WALLET
 DENOM_AMOUNT=$($CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $CONSUMER_WALLET -o json | jq -r '.balances | length')
 if [ $DENOM_AMOUNT -lt 2 ]; then
     echo "Only one denom found in consumer wallet."
@@ -31,11 +35,14 @@ fi
 echo "Found at least two denoms in the consumer wallet."
 
 # Transfer consumer token to provider chain
+echo "Current $CHAIN_ID bank blances: $PROVIDER_WALLET"
+$CHAIN_BINARY --home $HOME_1 q bank balances $PROVIDER_WALLET
 echo "Sending $CONSUMER_DENOM to $CHAIN_ID..."
-$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $PROVIDER_WALLET 1000$CONSUMER_DENOM --from $CONSUMER_WALLET --keyring-backend test -y -b sync
+$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $PROVIDER_WALLET 1000$CONSUMER_DENOM --from $CONSUMER_WALLET --keyring-backend test -y -b sync --gas 500000 --fees 5000$DENOM
 echo "Waiting for the transfer to reach the provider chain..."
 sleep 60
-$CHAIN_BINARY --home $HOME_1 q bank balances $PROVIDER_WALLET -o json
+echo "$CHAIN_ID bank blances after sending: $PROVIDER_WALLET"
+$CHAIN_BINARY --home $HOME_1 q bank balances $PROVIDER_WALLET
 DENOM_AMOUNT=$($CHAIN_BINARY --home $HOME_1 q bank balances $PROVIDER_WALLET -o json | jq -r '.balances | length')
 if [ $DENOM_AMOUNT -lt $EXPECTED_DENOMS ]; then
     echo "Found less than expected denoms in provider wallet."
