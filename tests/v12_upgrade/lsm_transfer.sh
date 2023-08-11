7ec0848d052bfc623816dea33f74285ac47e3537
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-11 14:31:24 -0400

    lsm transfer

--- tests/v12_upgrade/lsm_transfer.sh
+++ tests/v12_upgrade/lsm_transfer.sh
@@ -6,4 +6,12 @@ source tests/process_tx.sh
 tokenized_denom="$VALOPER_1/1"
 
 echo "Transferring token ownership record..."
-$CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq '.'
+record_id=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.id')
+owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
+echo "$owner owns record $record_id."
+submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_4 --from $owner --gas auto --gas_adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
+echo "$owner owns record $record_id."
+submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_3 --from $owner --gas auto --gas_adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
+echo "$owner owns record $record_id."
