561ce82b92d4ed0fa555e5e7e6666ed56b9a5959
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-11 20:52:39 -0400

    query after redeem

--- tests/v12_upgrade/lsm_complex.sh
+++ tests/v12_upgrade/lsm_complex.sh
@@ -106,21 +106,14 @@ echo "Unjailing validator 2..."
 tests/major_fresh_upgrade/unjail_validator.sh $PROVIDER_SERVICE_2 $VAL2_RPC_PORT $WALLET_2 $VALOPER_2
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 delegation_balance_pre_tokenize=$($CHAIN_BINARY q staking delegations $liquid_address_2 --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
+
 echo "Tokenizing with tokenizing account..."
 submit_tx "tx staking tokenize-share $VALOPER_2 $delegation_balance_pre_tokenize$DENOM $liquid_address_2 --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-
-val1=$($CHAIN_BINARY q staking validator $VALOPER_1 -o json --home $HOME_1 | jq '.jailed')
-val2=$($CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.jailed')
-val3=$($CHAIN_BINARY q staking validator $VALOPER_3 -o json --home $HOME_1 | jq '.jailed')
-echo "Validator jailed status: $val1 $val2 $val3"
+$CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 
 echo "Redeeming with tokenizing account..."
 submit_tx "tx staking redeem-tokens $delegation_balance_pre_tokenize$tokenized_denom_2 --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-
-val1=$($CHAIN_BINARY q staking validator $VALOPER_1 -o json --home $HOME_1 | jq '.jailed')
-val2=$($CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.jailed')
-val3=$($CHAIN_BINARY q staking validator $VALOPER_3 -o json --home $HOME_1 | jq '.jailed')
-echo "Validator jailed status: $val1 $val2 $val3"
+$CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 
 delegation_balance_post_redeem=$($CHAIN_BINARY q staking delegations $liquid_address_2 --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
 echo "Balance: $delegation_balance_post_redeem"
@@ -142,4 +135,4 @@ fi
 # delegation_balance=$($CHAIN_BINARY q staking delegations $bonding_address --home $HOME_1 -o json | jq -r '.delegation_responses[0].balance.amount')
 # submit_tx "tx staking unbond $VALOPER_2 ${delegation_balance%.*}$DENOM --from bonding_account -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
-$CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
+# $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
