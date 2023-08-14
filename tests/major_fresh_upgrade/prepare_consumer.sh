bc8b7dfdc2911de06f31de9c4fecd8bac03fa35e
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-14 09:38:56 -0400

    restore consumer a

--- tests/major_fresh_upgrade/prepare_consumer.sh
+++ tests/major_fresh_upgrade/prepare_consumer.sh
@@ -12,7 +12,7 @@ proposal="$CHAIN_BINARY tx gov submit-proposal consumer-addition proposal-add-$C
 echo $proposal
 txhash=$($proposal | jq -r .txhash)
 # Wait for the proposal to go on chain
-sleep 4
+sleep $(($COMMIT_TIMEOUT+2))
 
 # Get proposal ID from txhash
 echo "Getting proposal ID from txhash..."
@@ -21,10 +21,10 @@ proposal_id=$($CHAIN_BINARY q tx $txhash --home $HOME_1 --output json | jq -r '.
 
 echo "Voting on proposal $proposal_id..."
 $CHAIN_BINARY tx gov vote $proposal_id yes --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --from $WALLET_1 --keyring-backend test --home $HOME_1 --chain-id $CHAIN_ID -b block -y
+sleep $(($COMMIT_TIMEOUT+2))
 $CHAIN_BINARY q gov tally $proposal_id --home $HOME_1
 
 echo "Waiting for proposal to pass..."
-sleep 5
 sleep $VOTING_PERIOD
 
 #$CHAIN_BINARY q gov proposals --home $HOME_1
