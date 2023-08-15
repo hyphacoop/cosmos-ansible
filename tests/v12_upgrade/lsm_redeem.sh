580dfa1245dab6d50c25a18bb630616ea32f112f
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-15 10:12:06 -0400

    add ibc transfer to lsm test

--- tests/v12_upgrade/lsm_redeem.sh
+++ tests/v12_upgrade/lsm_redeem.sh
@@ -19,17 +19,17 @@ submit_tx "tx bank send $WALLET_1 $liquid_address 100000000uatom --from $WALLET_
 echo "Sending tokens from WALLET_3 to liquid_address via bank send..."
 submit_tx "tx bank send $WALLET_3 $liquid_address $bank_send_amount$tokenized_denom --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
 # sleep 2
-# echo "Sending tokens from $WALLET_3 to $WALLET_5 via ibc transfer..."
-# submit_tx "tx ibc-transfer transfer transfer channel-1 $STRIDE_WALLET_5 $ibc_transfer_amount$tokenized_denom --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
+echo "Sending tokens from $WALLET_3 to $WALLET_5 via ibc transfer..."
+submit_ibc_tx "tx ibc-transfer transfer transfer $IBC_CHANNEL $STRIDE_WALLET_5 $ibc_transfer_amount$tokenized_denom --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y" $CHAIN_BINARY $HOME_1
 # echo "Waiting for IBC tokens to reach $STRIDE_CHAIN_ID..."
 # sleep 20
-# ibc_denom=ibc/$($STRIDE_CHAIN_BINARY q ibc-transfer denom-hash transfer/channel-1/$tokenized_denom --home $STRIDE_HOME_1 -o json | jq -r '.hash')
-# ibc_balance=$($STRIDE_CHAIN_BINARY q bank balances $STRIDE_WALLET_5 --home $STRIDE_HOME_1 -o json | jq -r --arg DENOM "$ibc_denom" '.balances[] | select(.denom==$DENOM).amount')
-# echo "IBC-wrapped liquid token balance: $ibc_balance$ibc_denom"
-# if [[ $ibc_balance -ne $ibc_transfer_amount ]]; then
-#     echo "Tokenize unsuccessful: unexpected ibc-wrapped liquid token balance"
-#     exit 1
-# fi
+ibc_denom=ibc/$($STRIDE_CHAIN_BINARY q ibc-transfer denom-hash transfer/$IBC_CHANNEL/$tokenized_denom --home $STRIDE_HOME_1 -o json | jq -r '.hash')
+ibc_balance=$($STRIDE_CHAIN_BINARY q bank balances $STRIDE_WALLET_5 --home $STRIDE_HOME_1 -o json | jq -r --arg DENOM "$ibc_denom" '.balances[] | select(.denom==$DENOM).amount')
+echo "IBC-wrapped liquid token balance: $ibc_balance$ibc_denom"
+if [[ $ibc_balance -ne $ibc_transfer_amount ]]; then
+    echo "Tokenize unsuccessful: unexpected ibc-wrapped liquid token balance"
+    exit 1
+fi
 
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 echo "Redeeming tokens from WALLET_3..."
