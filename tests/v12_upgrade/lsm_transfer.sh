a778537ba232b0269f00693ce6aa9a2d71db275e
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-15 16:25:31 -0400

    replace relayer acct in tx tests

--- tests/v12_upgrade/lsm_transfer.sh
+++ tests/v12_upgrade/lsm_transfer.sh
@@ -4,16 +4,19 @@
 source tests/process_tx.sh
 
 tokenized_denom="$VALOPER_1/1"
+$CHAIN_BINARY keys add new_owner --home $HOME_1
+owner_address=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="acct_liquid").address')
+echo "New owner address: $owner_address"
 
 record_id=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.id')
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
 
-echo "Transferring token ownership record to WALLET_4..."
-submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_4 --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+echo "Transferring token ownership record to new_owner..."
+submit_tx "tx staking transfer-tokenize-share-record $record_id $owner_address --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
-if [[ "$owner" == "$WALLET_4" ]]; then
+if [[ "$owner" == "$owner_address" ]]; then
     echo "Token ownership transfer succeeded."
 else
     echo "Token ownership transfer failed."
