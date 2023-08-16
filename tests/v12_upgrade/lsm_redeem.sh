200efb466a9645f6d7b672a2ac2df1df3d8dc5d7
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-15 20:37:15 -0400

    fix redeem amount

--- tests/v12_upgrade/lsm_redeem.sh
+++ tests/v12_upgrade/lsm_redeem.sh
@@ -120,7 +120,7 @@ fi
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 
 echo "Validator unbond from WALLET_3..."
-submit_tx "tx staking unbond $VALOPER_1 80000000$DENOM --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+submit_tx "tx staking unbond $VALOPER_1 70000000$DENOM --from $WALLET_3 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
 validator_bond_shares=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.total_validator_bond_shares')
 echo "Validator bond shares: ${validator_bond_shares%.*}"
 if [[ ${validator_bond_shares%.*} -ne 0  ]]; then
