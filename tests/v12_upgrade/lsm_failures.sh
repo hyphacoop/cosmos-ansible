db7c70ed10e09fd71eda9320447f0d5a79a3a8ea
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-16 13:40:18 -0400

    print staking params post-upgrade

--- tests/v12_upgrade/lsm_failures.sh
+++ tests/v12_upgrade/lsm_failures.sh
@@ -32,7 +32,7 @@ $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 submit_tx "tx staking delegate $VALOPER_2 $liquid_2_delegations$DENOM --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 
-echo "Failure case 1: Attempt to tokenize with WALLET_4 (no validator bond)..."
+echo "Failure case 1: Attempt to tokenize with liquid_1 (no validator bond)..."
 submit_bad_tx "tx staking tokenize-share $VALOPER_1 20000000$DENOM $liquid_address_1 --from $liquid_address_1 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 submit_tx "tx staking validator-bond $VALOPER_1 --from $liquid_address_1 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
@@ -53,7 +53,7 @@ if [[ ${validator_bond_shares%.*} -ne $liquid_1_delegations  ]]; then
     exit 1
 fi
 
-echo "Failure case 2: Attempt to tokenize bond delegations with WALLET_4..."
+echo "Failure case 2: Attempt to tokenize bond delegations with Wliquid_1..."
 submit_bad_tx "tx staking tokenize-share $VALOPER_1 $liquid_1_delegations$DENOM $liquid_address_1 --from $liquid_address_1 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 validator_delegations=$($CHAIN_BINARY q staking validator $VALOPER_2 --home $HOME_1 -o json | jq -r '.delegator_shares')
@@ -61,7 +61,7 @@ validator_cap=$(echo "$validator_delegations*$validator_cap_param" | bc)
 echo "Validator_delegations: ${validator_delegations%.*}"
 echo "Validator shares cap: ${validator_cap%.*}"
 
-echo "Failure case 3: Attempt to tokenize with WALLET_5, breaching the validator liquid staking cap..."
+echo "Failure case 3: Attempt to tokenize with liquid_2, breaching the validator liquid staking cap..."
 submit_bad_tx "tx staking tokenize-share $VALOPER_2 100000000$DENOM $liquid_address_2 --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 bonded_tokens=$($CHAIN_BINARY q staking pool --home $HOME_1 -o json | jq -r '.bonded_tokens')
@@ -70,27 +70,27 @@ global_cap=$(echo "$bonded_tokens*$global_cap_param" | bc)
 echo "Global shares cap: ${global_cap%.*}"
 echo "Global staked: $global_staked"
 
-echo "Failure case 4: Attempt to tokenize with WALLET_5, breaching the global liquid staking cap..."
+echo "Failure case 4: Attempt to tokenize with liquid_2, breaching the global liquid staking cap..."
 submit_bad_tx "tx staking tokenize-share $VALOPER_1 140000000$DENOM $liquid_address_2 --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 echo "Failure case 5: Attempt to unbond validator bond, fails because it breaches the validator bond factor"
-echo "Tokenizing WALLET_5 delegations..."
+echo "Tokenizing liquid_2 delegations..."
 submit_tx "tx staking tokenize-share $VALOPER_1 $liquid_2_tokenize$DENOM $liquid_address_2 --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
-echo "Attempting to unbond from WALLET_4..."
+echo "Attempting to unbond from liquid_1..."
 submit_bad_tx "tx staking unbond $VALOPER_1 $liquid_1_delegations$DENOM --from $liquid_address_1 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
-echo "Redeeming tokens from WALLET_5..."
+echo "Redeeming tokens from liquid_2..."
 $CHAIN_BINARY q bank balances $liquid_address_2 --home $HOME_1 -o json | jq '.balances'
 submit_tx "tx staking redeem-tokens $liquid_2_tokenize$tokenized_denom --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 
-echo "Failure case 6: Attempt to tokenize with WALLET_5 after disabling tokenizing..."
+echo "Failure case 6: Attempt to tokenize with liquid_2 after disabling tokenizing..."
 submit_tx "tx staking disable-tokenize-shares --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 submit_bad_tx "tx staking tokenize-share $VALOPER_1 10000000$DENOM $liquid_address_2 --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 submit_tx "tx staking enable-tokenize-shares --from $liquid_address_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 
 # Cleanup
-echo "Unbonding delegations from WALLET_4 and WALLET_5..."
+echo "Unbonding delegations from liquid_1 and liquid_2..."
 submit_tx "tx staking unbond $VALOPER_1 $liquid_1_delegations$DENOM --from $liquid_address_1 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 submit_tx "tx staking unbond $VALOPER_2 $liquid_1_delegations$DENOM --from $liquid_address_1 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
