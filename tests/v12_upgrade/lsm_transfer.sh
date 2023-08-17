0a90fd8265821e65c5d5d6f42279a31fba68c051
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-17 09:12:25 -0400

    add logging for lsm happy path

--- tests/v12_upgrade/lsm_transfer.sh
+++ tests/v12_upgrade/lsm_transfer.sh
@@ -16,7 +16,9 @@ owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom 
 echo "$owner owns record $record_id."
 
 echo "Transferring token ownership record to new_owner..."
+tests/v12_upgrade/log_lsm_data.sh happy pre-transfer-1 $owner_address -
 submit_tx "tx staking transfer-tokenize-share-record $record_id $owner_address --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+tests/v12_upgrade/log_lsm_data.sh happy post-transfer-1 $owner_address -
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
 if [[ "$owner" == "$owner_address" ]]; then
@@ -26,7 +28,9 @@ else
 fi
 
 echo "Transferring token ownership record back to WALLET_3..."
+tests/v12_upgrade/log_lsm_data.sh happy pre-transfer-2 $owner -
 submit_tx "tx staking transfer-tokenize-share-record $record_id $WALLET_3 --from $owner --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+tests/v12_upgrade/log_lsm_data.sh happy post-transfer-2 $owner -
 owner=$($CHAIN_BINARY q staking tokenize-share-record-by-denom $tokenized_denom --home $HOME_1 -o json | jq -r '.record.owner')
 echo "$owner owns record $record_id."
 if [[ "$owner" == "$WALLET_3" ]]; then
--- tests/v12_upgrade/lsm_validator_bond.sh
+++ tests/v12_upgrade/lsm_validator_bond.sh
@@ -5,9 +5,8 @@ delegation=100000000
 delegator_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.delegator_shares')
 validator_bond_shares_1=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.validator_bond_shares')
 
-# Log data
-tests/v12_upgrade/log_lsm_data.sh happy pre-delegate $WALLET_2 $delegation
 echo "Delegating with $WALLET_2..."
+tests/v12_upgrade/log_lsm_data.sh happy pre-delegate $WALLET_2 $delegation
 submit_tx "tx staking delegate $VALOPER_1 $delegation$DENOM --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y" $CHAIN_BINARY $HOME_1
 tests/v12_upgrade/log_lsm_data.sh happy post-delegate $WALLET_2 $delegation
 
@@ -21,9 +20,10 @@ fi
 
 $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
 
-tests/v12_upgrade/log_lsm_data.sh happy pre-bond $WALLET_2 -
 echo "Validator bond with $WALLET_2..."
+tests/v12_upgrade/log_lsm_data.sh happy pre-bond $WALLET_2 -
 submit_tx "tx staking validator-bond $VALOPER_1 --from $WALLET_2 -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT -y --fees $BASE_FEES$DENOM" $CHAIN_BINARY $HOME_1
+tests/v12_upgrade/log_lsm_data.sh happy post-bond $WALLET_2 -
 
 validator_bond_shares_2=$($CHAIN_BINARY q staking validator $VALOPER_1 --home $HOME_1 -o json | jq -r '.validator_bond_shares')
 bond_shares_diff=$((${validator_bond_shares_2%.*}-${validator_bond_shares_1%.*})) # remove decimal portion
