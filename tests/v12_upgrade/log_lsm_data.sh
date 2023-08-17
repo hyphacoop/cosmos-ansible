535019aea6aeb3b08e6e25733f53f711d70a361f
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-17 10:59:15 -0400

    log account details

--- tests/v12_upgrade/log_lsm_data.sh
+++ tests/v12_upgrade/log_lsm_data.sh
@@ -18,4 +18,11 @@ val2_shares=$(echo $val2_data | jq -r '.delegator_shares')
 val2_bond_shares=$(echo $val2_data | jq -r '.validator_bond_shares')
 val2_liquid_shares=$(echo $val2_data | jq -r '.liquid_shares')
 
-echo "$flow,$action,$account,$amount,$val1_tokens,$val1_shares,$val1_bond_shares,$val1_liquid_shares,$val2_tokens,$val2_shares,$val2_bond_shares,$val2_liquid_shares" >> $LSM_LOG
+acct_balances=$($CHAIN_BINARY q bank balances $account --home $HOME_1 -o json)
+acct_delegations=$($CHAIN_BINARY q staking delegations $account --home $HOME_1 -o json)
+
+balance=$(echo $acct_balances | jq -r '.balances[0].amount')$(echo $acct_balance | jq -r '.balances[0].denom')
+del_shares=$(echo $acct_delegations | jq -r '.delegation_responses[0].delegation.shares')
+del_balance=$(echo $acct_delegations | jq -r '.delegation_responses[0].balance.amount')$(echo $acct_delegations | jq -r '.delegation_responses[0].balance.denom')
+
+echo "$flow,$action,$account,$amount,$balance,$del_shares,$del_balance,$val1_tokens,$val1_shares,$val1_bond_shares,$val1_liquid_shares,$val2_tokens,$val2_shares,$val2_bond_shares,$val2_liquid_shares" >> $LSM_LOG
