d893ec58a906602e2065fc27815fbaccf5fea497
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-16 21:51:04 -0400

    async tx

--- tests/v12_upgrade/lsm_accounting.sh
+++ tests/v12_upgrade/lsm_accounting.sh
@@ -132,8 +132,8 @@ $CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.'
 $CHAIN_BINARY q staking delegations $bonding_address -o json --home $HOME_1 | jq '.'
 
 echo "Decreasing delegation from validator bond delegator..."
-submit_tx "tx staking unbond $VALOPER_2 5000000$DENOM --from $bonding_address -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees 5000$DENOM -y" $CHAIN_BINARY $HOME_1
-sleep 5
+$CHAIN_BINARY tx staking unbond $VALOPER_2 $tokenize$DENOM --from $bonding_address -o json --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -y -b async --home $HOME_1
+sleep 20
 $CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq '.'
 $CHAIN_BINARY q staking validator $VALOPER_3 -o json --home $HOME_1 | jq '.'
 $CHAIN_BINARY q staking delegations $bonding_address -o json --home $HOME_1 | jq '.'
