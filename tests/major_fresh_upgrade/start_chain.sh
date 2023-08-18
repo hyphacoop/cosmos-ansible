3e5312ab7a6f326205c778285ba61144f710df21
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-08-18 13:45:22 -0400

    restore main build url

--- tests/major_fresh_upgrade/start_chain.sh
+++ tests/major_fresh_upgrade/start_chain.sh
@@ -3,7 +3,6 @@
 
 # Install Gaia binary
 CHAIN_BINARY_URL=https://github.com/cosmos/gaia/releases/download/$START_VERSION/gaiad-$START_VERSION-linux-amd64
-# CHAIN_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/gaiad-test/gaiad-lsm-bugfix
 echo "Installing Gaia..."
 wget $CHAIN_BINARY_URL -O $HOME/go/bin/$CHAIN_BINARY -q
 chmod +x $HOME/go/bin/$CHAIN_BINARY
