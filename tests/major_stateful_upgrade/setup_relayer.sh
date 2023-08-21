a0b0e755d749520e854852ac6d3ecba558d50e2a
ASoTNetworks <dev@aseriesoftubez.com>
2023-08-21 14:00:30 -0400

    hermes use wallet 4, set LSM params

--- tests/major_stateful_upgrade/setup_relayer.sh
+++ tests/major_stateful_upgrade/setup_relayer.sh
@@ -38,7 +38,7 @@ echo "Setting up Hermes config..."
 cp tests/major_stateful_upgrade/hermes-config.toml ~/.hermes/config.toml
 
 echo "Adding relayer keys..."
-echo $MNEMONIC_1 > mnemonic.txt
+echo $MNEMONIC_4 > mnemonic.txt
 hermes keys add --chain $CHAIN_ID --mnemonic-file mnemonic.txt
 hermes keys add --chain consumera --mnemonic-file mnemonic.txt
 hermes keys add --chain consumerb --mnemonic-file mnemonic.txt
