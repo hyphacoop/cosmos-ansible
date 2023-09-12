b4ccaf678e4f411658226be8dda55c39ceac9d67
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-09-12 09:52:26 -0400

    upgrade consumer chain

---
+++ tests/ics_upgrade/upgrade_consumer_v3.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Upgrade a consumer chain
+
+echo "Disable services..."
+sudo systemctl disable $CONSUMER_SERVICE_1 --now
+sudo systemctl disable $CONSUMER_SERVICE_2 --now
+
+echo "Replace binary in services..."
+
+sudo rm /etc/systemd/system/$CONSUMER_SERVICE_1
+sudo touch /etc/systemd/system/$CONSUMER_SERVICE_1
+echo "[Unit]"                               | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1
+echo "Description=Consumer service"       | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "After=network-online.target"          | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo ""                                     | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "[Service]"                            | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "User=$USER"                            | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "ExecStart=$HOME/go/bin/$CONSUMER_CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $CONSUMER_HOME_1" | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "Restart=no"                       | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo ""                                     | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "[Install]"                            | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_1 -a
+
+sudo rm /etc/systemd/system/$CONSUMER_SERVICE_2
+sudo touch /etc/systemd/system/$CONSUMER_SERVICE_2
+echo "[Unit]"                               | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2
+echo "Description=Consumer service"       | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "After=network-online.target"          | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo ""                                     | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "[Service]"                            | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "User=$USER"                            | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "ExecStart=$HOME/go/bin/$CONSUMER_CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $CONSUMER_HOME_2" | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "Restart=no"                       | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo ""                                     | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "[Install]"                            | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$CONSUMER_SERVICE_2 -a
+
+echo "Restart services..."
+sudo systemctl daemon-reload
+
+sudo systemctl enable $CONSUMER_SERVICE_1 --now
+sudo systemctl enable $CONSUMER_SERVICE_2 --now
+
+sleep 10
