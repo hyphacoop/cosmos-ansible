4fc1662228f1ae8a037d4efe997c48b223158245
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-09-14 22:54:41 -0400

    print tx

--- tests/v13_upgrade/test_denom_rewards_tx.sh
+++ tests/v13_upgrade/test_denom_rewards_tx.sh
@@ -11,7 +11,9 @@ reward=$($CHAIN_BINARY q distribution rewards $WALLET_1 --home $HOME_1 -o json |
 echo "Before: $reward"
 
 echo "Registering denom"
-$CHAIN_BINARY tx provider register-consumer-reward-denom $hash --from $WALLET_1 --gas auto --gas-adjustment 1.2 --fees 1000$DENOM --home $HOME_1 -b block -y
+tx="$CHAIN_BINARY tx provider register-consumer-reward-denom $hash --from $WALLET_1 --gas auto --gas-adjustment 1.2 --fees 1000$DENOM --home $HOME_1 -b block -y"
+echo $tx
+$tx
 sleep 10
 echo "Denom registered"
 reward=$($CHAIN_BINARY q distribution rewards $WALLET_1 --home $HOME_1 -o json | jq -r --arg DENOM "$hash" '.total[] | select(.denom==$DENOM).amount')
