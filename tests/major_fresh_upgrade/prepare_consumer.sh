8af8f38dc11a8662b809cdcfc967521437b766f4
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-09-15 10:49:14 -0400

    ics v1.2.0 patch

--- tests/major_fresh_upgrade/prepare_consumer.sh
+++ tests/major_fresh_upgrade/prepare_consumer.sh
@@ -33,9 +33,16 @@ echo "Collecting the CCV state..."
 $CHAIN_BINARY q provider consumer-genesis $CONSUMER_CHAIN_ID -o json --home $HOME_1 > ccv-pre.json
 jq '.params |= . + {"soft_opt_out_threshold": "0.10"}' ccv-pre.json > ccv-optout.json
 
+
 echo "Patching the CCV state with the provider reward denom"
 jq --arg DENOM "$CONSUMER_DENOM" '.params.reward_denoms = [$DENOM]' ccv-optout.json > ccv-reward.json
 cp ccv-reward.json ccv.json
+
+if $ICS_120 ; then
+    echo "Patching for ICS v1.2.0"
+    jq 'del(.preCCV)' ccv.json > ccv-120.json
+    cp ccv-12.json ccv.json
+fi
 
 echo "Patching the consumer genesis file..."
 jq -s '.[0].app_state.ccvconsumer = .[1] | .[0]' $CONSUMER_HOME_1/config/genesis.json ccv.json > consumer-genesis.json
