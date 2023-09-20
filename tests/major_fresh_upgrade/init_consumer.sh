00ed660ae233c18bcd9f63a62fa243f2d86747e1
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-09-19 22:14:28 -0400

    commit timeout units

--- tests/major_fresh_upgrade/init_consumer.sh
+++ tests/major_fresh_upgrade/init_consumer.sh
@@ -106,9 +106,10 @@ toml set --toml-path $CONSUMER_HOME_2/config/config.toml p2p.persistent_peers "$
 toml set --toml-path $CONSUMER_HOME_3/config/config.toml p2p.persistent_peers "$VAL1_PEER"
 
 echo "Setting a short commit timeout..."
-toml set --toml-path $CONSUMER_HOME_1/config/config.toml consensus.timeout_commit "$COMMIT_TIMEOUT"
-toml set --toml-path $CONSUMER_HOME_2/config/config.toml consensus.timeout_commit "$COMMIT_TIMEOUT"
-toml set --toml-path $CONSUMER_HOME_2/config/config.toml consensus.timeout_commit "$COMMIT_TIMEOUT"
+seconds=s
+toml set --toml-path $CONSUMER_HOME_1/config/config.toml consensus.timeout_commit "$COMMIT_TIMEOUT$seconds"
+toml set --toml-path $CONSUMER_HOME_2/config/config.toml consensus.timeout_commit "$COMMIT_TIMEOUT$seconds"
+toml set --toml-path $CONSUMER_HOME_2/config/config.toml consensus.timeout_commit "$COMMIT_TIMEOUT$seconds"
 
 # Set fast_sync to false - or block_sync for ICS v3
 toml set --toml-path $CONSUMER_HOME_1/config/config.toml fast_sync false
