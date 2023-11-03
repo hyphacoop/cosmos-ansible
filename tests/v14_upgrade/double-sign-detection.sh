42800aaedc505310d611ceda94909a62e2f9d9e6
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-11-03 07:53:37 -0400

    fix validator home

--- tests/v14_upgrade/double-sign-detection.sh
+++ tests/v14_upgrade/double-sign-detection.sh
@@ -275,7 +275,7 @@ sleep 60
 
 journalctl -u hermes-evidence
 
-status=$($CHAIN_BINARY q slashing signing-info $($CHAIN_BINARY tendermint show-validator --home $HOME_1) --home $HOME_1 -o json | jq '.tombstoned')
+status=$($CHAIN_BINARY q slashing signing-info $($CHAIN_BINARY tendermint show-validator --home $EQ_PROVIDER_HOME) --home $HOME_1 -o json | jq '.tombstoned')
 echo "Status: $status"
 if [ $status == "true" ]; then
   echo "Success: validator has been tombstoned!"
