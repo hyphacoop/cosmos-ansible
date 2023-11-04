ffcbaaa00cc9e67221bcc5017c8bb37b63dc1d4d
ASoTNetworks <dev@aseriesoftubez.com>
2023-11-04 02:58:34 -0400

    wait longer for ccv

--- tests/major_stateful_upgrade/test_ccv.sh
+++ tests/major_stateful_upgrade/test_ccv.sh
@@ -6,7 +6,7 @@ echo "Delegating additional stake to $MONIKER_1..."
 $CHAIN_BINARY --home $HOME_1 tx staking delegate $VALOPER_1 $DELEGATE_2_AMOUNT$DENOM --from $MONIKER_2 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y --chain-id $CHAIN_ID
 # Wait for consumer chain to get validator set update
 echo "Waiting for the validator set update to reach the consumer chain..."
-sleep 120
+sleep 300
 PROVIDER_BADDRESS=$(jq -r '.address' $HOME_1/config/priv_validator_key.json)
 PROVIDER_POWER=$(curl -s http://localhost:$VAL1_RPC_PORT/validators | jq -r '.result.validators[] | select(.address=="'$PROVIDER_BADDRESS'") | '.voting_power'')
 
