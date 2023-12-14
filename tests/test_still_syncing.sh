9b9641d9b4cc6bfe1001330e298498415338d125
ASoTNetworks <dev@aseriesoftubez.com>
2023-12-14 13:41:12 -0500

    dynamically set host / port

--- tests/test_still_syncing.sh
+++ tests/test_still_syncing.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+host=$1
+port=$2
+
 still_syncing="true"
 while [ $still_syncing == "true" ]
 do
-	still_syncing=$(curl -s 127.0.0.1:26657/status | jq -r .result.sync_info.still_syncing)
+	still_syncing=$(curl -s ${host}:${port}/status | jq -r .result.sync_info.catching_up)
 	echo "Still Syncing: $still_syncing"
 	sleep 5
 done
