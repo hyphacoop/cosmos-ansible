3c2f9cb6b187d74fb2fda27116f20da148fb63e4
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-04 11:13:49 -0500

    decrease ICA delegation

--- tests/v12_upgrade/ica_delegate_happy.sh
+++ tests/v12_upgrade/ica_delegate_happy.sh
@@ -3,7 +3,7 @@
 
 source tests/process_tx.sh
 
-delegate=20000000
+delegate=10000000
 bond_delegation=20000000
 
 $CHAIN_BINARY keys add lsp_happy_bonding --home $HOME_1
@@ -71,11 +71,14 @@ echo "** LIQUID STAKING PROVIDER HAPPY PATH> 2: DELEGATE VIA ICA **"
     echo "Sending tx staking delegate to host chain..."
     tests/v12_upgrade/log_lsm_data.sh lsp-happy pre-ica-delegate-1 $ICA_ADDRESS $delegate
     # submit_ibc_tx "tx interchain-accounts controller send-tx connection-0 delegate_packet.json --from $STRIDE_WALLET_1 --chain-id $STRIDE_CHAIN_ID --gas auto --fees $BASE_FEES$STRIDE_DENOM --gas-adjustment $GAS_ADJUSTMENT -y -o json" $STRIDE_CHAIN_BINARY $STRIDE_HOME_1
+    $CHAIN_BINARY q bank balances $ICA_ADDRESS --home $HOME_1
+
     strided tx interchain-accounts controller send-tx connection-0 delegate_packet.json --from $STRIDE_WALLET_1 --chain-id $STRIDE_CHAIN_ID --gas auto --fees $BASE_FEES$STRIDE_DENOM --gas-adjustment $GAS_ADJUSTMENT -y -o json --home $STRIDE_HOME_1
     echo "Waiting for delegation to go on-chain..."
     sleep $(($COMMIT_TIMEOUT*8))
     tests/v12_upgrade/log_lsm_data.sh lsp-happy post-ica-delegate-1 $ICA_ADDRESS $delegate
     
+    $CHAIN_BINARY q bank balances $ICA_ADDRESS --home $HOME_1
     $CHAIN_BINARY q staking validators -o json --home $HOME_1 | jq '.'
     post_delegation_tokens=$($CHAIN_BINARY q staking validator $VALOPER_2 -o json --home $HOME_1 | jq -r '.tokens')
     echo "Post-delegation val tokens: $post_delegation_tokens"
@@ -95,7 +98,7 @@ echo "** LIQUID STAKING PROVIDER HAPPY PATH> 2: DELEGATE VIA ICA **"
     elif [[ $(($delegate-$tokens_delta)) -eq 1 ]]; then
         echo "Delegation success: tokens increase off by 1"
     else
-        echo "Accounting failure: unexpected tokens decrease ($tokens_delta != $delegate)"
+        echo "Accounting failure: unexpected tokens increase ($tokens_delta != $delegate)"
         exit 1
     fi
     
