e4b3a81f640259f2509277573133a5bd5784ad1c
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-17 16:00:46 -0500

    tx hash

--- tests/test_tx_fresh.sh
+++ tests/test_tx_fresh.sh
@@ -4,6 +4,7 @@
 check_code()
 {
   txhash=$1
+  echo "Querying hash $txhash..."
   code=$($CHAIN_BINARY q tx $txhash -o json --home $HOME_1 | jq '.code')
   if [ $code -ne 0 ]; then
     echo "tx was unsuccessful."
@@ -13,12 +14,13 @@ check_code()
 }
 
 echo "Sending funds with tx bank send..."
-TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 $VAL_STAKE_STEP$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq '.txhash' | tr -d '"')
+TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 $VAL_STAKE_STEP$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq -r '.txhash')
+echo "Tx hash: $TXHASH"
 check_code $TXHASH
 sleep $COMMIT_TIMEOUT
 
 echo "Delegating funds from test account to validator..."
-TXHASH=$($CHAIN_BINARY tx staking delegate $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq '.txhash' | tr -d '"')
+TXHASH=$($CHAIN_BINARY tx staking delegate $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq -r '.txhash')
 check_code $TXHASH
 sleep $COMMIT_TIMEOUT
 
@@ -27,7 +29,7 @@ sleep 20
 echo "Withdrawing rewards for test account..."
 starting_balance=$($CHAIN_BINARY q bank balances $WALLET_1 --home $HOME_1 -o json | jq -r '.balances[] | select(.denom=="uatom").amount')
 echo "Starting balance: $starting_balance"
-TXHASH=$($CHAIN_BINARY tx distribution withdraw-rewards $VALOPER_1 --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq '.txhash' | tr -d '"')
+TXHASH=$($CHAIN_BINARY tx distribution withdraw-rewards $VALOPER_1 --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq -r '.txhash')
 check_code $TXHASH
 
 # Check the funds again
@@ -45,7 +47,7 @@ fi
 $CHAIN_BINARY q staking validators --home $HOME_1
 
 echo "Unbonding funds from test account to validator..."
-TXHASH=$($CHAIN_BINARY tx staking unbond $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $HIGH_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq '.txhash' | tr -d '"')
+TXHASH=$($CHAIN_BINARY tx staking unbond $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $HIGH_FEES$DENOM --chain-id $CHAIN_ID -y -o json | jq -r '.txhash')
 check_code $TXHASH
 
 $CHAIN_BINARY q staking validators --home $HOME_1
