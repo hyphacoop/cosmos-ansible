4eb9545ae9e123ea13ce22d76d3f6b6aaf8040a6
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-17 21:19:20 -0500

    key migration

--- tests/v15_upgrade/flush_key_migration.sh
+++ tests/v15_upgrade/flush_key_migration.sh
@@ -6,4 +6,4 @@ $CHAIN_BINARY tx bank send $WALLET_1 $WALLET_1 1$DENOM --home $HOME_1 --from $WA
 $CHAIN_BINARY tx bank send $WALLET_2 $WALLET_2 1$DENOM --home $HOME_1 --from $WALLET_2 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
 $CHAIN_BINARY tx bank send $WALLET_3 $WALLET_3 1$DENOM --home $HOME_1 --from $WALLET_3 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
 $CHAIN_BINARY tx bank send $WALLET_4 $WALLET_4 1$DENOM --home $HOME_1 --from $WALLET_4 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-$CHAIN_BINARY tx bank send $WALLET_RELAYER $WALLET_RELAYER 1$DENOM --home $HOME_1 --from $WALLET_RELAYER --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+sleep $(($COMMIT_TIMEOUT+2))
