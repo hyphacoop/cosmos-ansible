5f768799acc8345c019579e16a6fe8407bb7ebb7
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-22 10:55:20 -0500

    check hash

--- tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
+++ tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
@@ -22,9 +22,11 @@ echo "Balances before:"
 $CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json
 DENOM_BEFORE=$($CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json | jq -r '.balances | length')
 echo "Sending $CONSUMER_DENOM to $CHAIN_ID..."
-$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $WALLET_1 1000$CONSUMER_DENOM --from $RECIPIENT --keyring-backend test --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$CONSUMER_DENOM -y
+command="$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 tx ibc-transfer transfer transfer channel-1 $WALLET_1 1000$CONSUMER_DENOM --from $RECIPIENT --keyring-backend test --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$CONSUMER_DENOM -y"
+txhash=$($proposal | jq -r .txhash)
+$CHAIN_BINARY q tx $txhash --home $CONSUMER_HOME_1
 echo "Waiting for the transfer to reach the provider chain..."
-sleep $(($COMMIT_TIMEOUT*10))
+sleep $(($COMMIT_TIMEOUT*15))
 echo "Balances after:"
 $CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json
 DENOM_AFTER=$($CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json | jq -r '.balances | length')
