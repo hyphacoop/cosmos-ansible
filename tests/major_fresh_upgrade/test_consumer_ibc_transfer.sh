223659e5090425409ccd7759fb2fc61561609e7d
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-22 11:59:33 -0500

    print relayer log

--- tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
+++ tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
@@ -9,10 +9,11 @@ PROVIDER_CHANNEL=$1
 echo "Sending $DENOM to $CONSUMER_CHAIN_ID..."
 $CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer $PROVIDER_CHANNEL $RECIPIENT 1000$DENOM --from $WALLET_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y
 echo "Waiting for the transfer to reach the consumer chain..."
-sleep $(($COMMIT_TIMEOUT*5))
+sleep $(($COMMIT_TIMEOUT*15))
 DENOM_AMOUNT=$($CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $RECIPIENT -o json | jq -r '.balances | length')
 if [ $DENOM_AMOUNT -lt 2 ]; then
     echo "Only one denom found in consumer wallet."
+    journalctl -u $RELAYER | tail -n 100
     exit 1
 fi
 echo "Found at least two denoms in the consumer wallet."
@@ -30,8 +31,10 @@ $CHAIN_BINARY q tx $txhash --home $CONSUMER_HOME_1
 echo "Balances after:"
 $CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json
 DENOM_AFTER=$($CHAIN_BINARY --home $HOME_1 q bank balances $WALLET_1 -o json | jq -r '.balances | length')
+
 if [ $DENOM_BEFORE -eq $DENOM_AFTER ]; then
     echo "The number of unique denoms in the provider wallet did not change."
+    journalctl -u $RELAYER | tail -n 100
     exit 1
 fi
 echo "The number of unique denoms in the provider wallet increase."
