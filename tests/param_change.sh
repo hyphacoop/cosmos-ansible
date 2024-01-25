1a78d16d4c66bc9e442b23a5ef370aed1a408f3b
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-25 12:16:49 -0500

    wait for block production

--- tests/param_change.sh
+++ tests/param_change.sh
@@ -2,13 +2,13 @@
 
 proposal_json=$1
 
-proposal="$CHAIN_BINARY tx gov submit-proposal param-change $proposal_json --from $WALLET_1 --home $HOME_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -o json -b block -y"
+proposal="$CHAIN_BINARY tx gov submit-proposal param-change $proposal_json --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y"
 echo $proposal
 txhash=$($proposal | jq -r .txhash)
 sleep $(( $COMMIT_TIMEOUT*2 ))
 echo "Proposal hash: $txhash"
 proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
-vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --home $HOME_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM -b block -y -o json"
+vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json"
 txhash=$($vote | jq -r .txhash)
 sleep $(( $COMMIT_TIMEOUT*2 ))
 echo "Vote hash: $txhash"
