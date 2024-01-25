fb2171bb1df7b7e4547e70f8e3d623e358d7ca63
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-25 15:43:48 -0500

    globalfee syntax

--- tests/test_globalfee.sh
+++ tests/test_globalfee.sh
@@ -6,7 +6,7 @@
 
 # 1. globalfee < node
 # set globalfee = 0.001uatom
-jq '.changes[0].value[0].amount = 0.001' tests/major_fresh_upgrade/globalfee-params.json > globalfee-1.json
+jq '.changes[0].value[0].amount = "0.001"' tests/major_fresh_upgrade/globalfee-params.json > globalfee-1.json
 tests/param_change.sh globalfee-1.json
 amount=$($CHAIN_BINARY q globalfee params -o json | jq -r --arg DENOM "$DENOM" '.minimum_gas_prices[] | select(.denom == $DENOM).amount'
 echo "globalfee minimum_gas_prices: $amount$DENOM"
@@ -19,7 +19,7 @@ echo "globalfee minimum_gas_prices: $amount$DENOM"
 
 # 2. set node < globalfee
 # globalfee = 0.009uatom
-jq '.changes[0].value[0].amount = 0.009' tests/major_fresh_upgrade/globalfee-params.json > globalfee-1.json
+jq '.changes[0].value[0].amount = "0.009"' tests/major_fresh_upgrade/globalfee-params.json > globalfee-1.json
 tests/param_change.sh globalfee-1.json
 amount=$($CHAIN_BINARY q globalfee params -o json | jq -r --arg DENOM "$DENOM" '.minimum_gas_prices[] | select(.denom == $DENOM).amount'
 echo "globalfee minimum_gas_prices: $amount$DENOM"
