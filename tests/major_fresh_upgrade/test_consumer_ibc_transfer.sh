4faa4913b683105839fedaf65d458d74bc3a08a0
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-24 23:08:33 -0500

    ibc balance check

--- tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
+++ tests/major_fresh_upgrade/test_consumer_ibc_transfer.sh
@@ -4,12 +4,38 @@
 echo "Running with $CONSUMER_CHAIN_BINARY."
 
 PROVIDER_CHANNEL=$1
+consumer_expected_denom=ibc/$(echo -n transfer/channel-1/uatom | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
+provider_expected_denom=ibc/$(echo -n transfer/$PROVIDER_CHANNEL/ucon | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
+echo "expected denom in provider: $provider_expected_denom"
+echo "expected denom in consumer: $consumer_expected_denom"
+
+echo "Testing denom check..."
+consumer_start_balance=$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $RECIPIENT -o json | jq -r --arg DENOM "$consumer_expected_denom" '.balances[] | select(.denom==$DENOM).amount'
+if [ -z "$consumer_start_balance" ]; then
+  consumer_start_balance=0
+fi
+echo "Consumer starting balance in expected denom: $consumer_start_balance"
 
 # Transfer provider token to consumer chain
 echo "Sending $DENOM to $CONSUMER_CHAIN_ID..."
 $CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer $PROVIDER_CHANNEL $RECIPIENT 1000$DENOM --from $WALLET_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y
 echo "Waiting for the transfer to reach the consumer chain..."
 sleep $(($COMMIT_TIMEOUT*15))
+$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances
+
+echo "Testing denom check..."
+consumer_end_balance=$CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $RECIPIENT -o json | jq -r --arg DENOM "$consumer_expected_denom" '.balances[] | select(.denom==$DENOM).amount'
+if [ -z "$consumer_end_balance" ]; then
+  consumer_end_balance=0
+fi
+echo "Consumer ending balance in expected denom: $consumer_end_balance"
+
+if [ $consumer_end_balance -gt $consumer_start_balance ]; then
+  echo "Consumer balance has increased!"
+else
+  echo "Consumer balance has not increased!"
+fi
+
 DENOM_AMOUNT=$($CONSUMER_CHAIN_BINARY --home $CONSUMER_HOME_1 q bank balances $RECIPIENT -o json | jq -r '.balances | length')
 if [ $DENOM_AMOUNT -lt 2 ]; then
     echo "Only one denom found in consumer wallet."
