d6a2cf976e1749b029121eaae89109271680536c
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-26 07:20:35 -0500

    change proposal after upgrade

--- tests/param_change.sh
+++ tests/param_change.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 proposal_json=$1
-# if [[ $COSMOS_SDK == "v45"]]; then
+if [[ $COSMOS_SDK == "v45"]]; then
 proposal="$CHAIN_BINARY tx gov submit-proposal param-change $proposal_json --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y"
-# elif [[ $COSMOS_SDK == "v47"]]; then
-# proposal="$CHAIN_BINARY tx gov submit-proposal param-change $proposal_json --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y"
-# fi
+elif [[ $COSMOS_SDK == "v47"]]; then
+proposal="$CHAIN_BINARY tx gov submit-legacy-proposal param-change $proposal_json --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y"
+fi
 echo $proposal
 txhash=$($proposal | jq -r .txhash)
 sleep $(( $COMMIT_TIMEOUT*2 ))
