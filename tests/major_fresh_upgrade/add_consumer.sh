31eb681cc7bc04b9603a9029723ae247a9669514
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-28 00:53:52 -0500

    proposal path

--- tests/major_fresh_upgrade/add_consumer.sh
+++ tests/major_fresh_upgrade/add_consumer.sh
@@ -5,9 +5,9 @@ spawn_time=$(date -u --iso-8601=ns -d '30 secs' | sed s/+00:00/Z/ | sed s/,/./) 
 jq -r --arg SPAWNTIME "$spawn_time" '.spawn_time |= $SPAWNTIME' tests/patch_upgrade/proposal-add-template.json > proposal-add-spawn.json
 
 if $STRIDE_HEIGHT_OFFSET ; then
-    jq -r --argjson HEIGHT "$STRIDE_REV_HEIGHT" '.initial_height.revision_height |= $HEIGHT' proposal-add-template.json > proposal-add-height.json
+    jq -r --argjson HEIGHT "$STRIDE_REV_HEIGHT" '.initial_height.revision_height |= $HEIGHT' tests/patch_upgrade/proposal-add-template.json > proposal-add-height.json
     spawn_time=$(date -u --iso-8601=ns | sed s/+00:00/Z/ | sed s/,/./)
-    jq -r --arg SPAWNTIME "$spawn_time" '.spawn_time |= $SPAWNTIME' tests/patch_upgrade/proposal-add-height.json > proposal-add-spawn.json
+    jq -r --arg SPAWNTIME "$spawn_time" '.spawn_time |= $SPAWNTIME' proposal-add-height.json > proposal-add-spawn.json
 fi
 
 sed "s%\"chain_id\": \"\"%\"chain_id\": \"$CONSUMER_CHAIN_ID\"%g" proposal-add-spawn.json > proposal-add-$CONSUMER_CHAIN_ID.json
