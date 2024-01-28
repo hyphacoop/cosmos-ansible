0611e3d473039c70cdffe9463e8e6797c1d5e070
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-28 00:47:27 -0500

    set revision height

--- tests/major_fresh_upgrade/add_consumer.sh
+++ tests/major_fresh_upgrade/add_consumer.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 echo "Patching add template with spawn time..."
-spawn_time=$(date -u --iso-8601=ns | sed s/+00:00/Z/ | sed s/,/./)
-# spawn_time=$(date -u --iso-8601=ns -d '30 secs' | sed s/+00:00/Z/ | sed s/,/./) # 30 seconds in the future
+spawn_time=$(date -u --iso-8601=ns -d '30 secs' | sed s/+00:00/Z/ | sed s/,/./) # 30 seconds in the future
 jq -r --arg SPAWNTIME "$spawn_time" '.spawn_time |= $SPAWNTIME' tests/patch_upgrade/proposal-add-template.json > proposal-add-spawn.json
+
+if $STRIDE_HEIGHT_OFFSET ; then
+    jq -r --argjson HEIGHT "$STRIDE_REV_HEIGHT" '.initial_height.revision_height |= $HEIGHT' proposal-add-template.json > proposal-add-height.json
+    spawn_time=$(date -u --iso-8601=ns | sed s/+00:00/Z/ | sed s/,/./)
+    jq -r --arg SPAWNTIME "$spawn_time" '.spawn_time |= $SPAWNTIME' tests/patch_upgrade/proposal-add-height.json > proposal-add-spawn.json
+fi
+
 sed "s%\"chain_id\": \"\"%\"chain_id\": \"$CONSUMER_CHAIN_ID\"%g" proposal-add-spawn.json > proposal-add-$CONSUMER_CHAIN_ID.json
 rm proposal-add-spawn.json
 
