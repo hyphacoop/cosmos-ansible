3fa68f55cc6440a71b621e69aff2cd941aad200f
ASoTNetworks <dev@aseriesoftubez.com>
2024-01-28 00:29:38 -0500

    fix output vars

--- tests/major_fresh_upgrade/test_clawback.sh
+++ tests/major_fresh_upgrade/test_clawback.sh
@@ -9,13 +9,13 @@ let post_upgrade_height=$upgrade_height+1
 pre_upgrade_acc_type=$(gaiad --home $HOME_1 q account cosmos145hytrc49m0hn6fphp8d5h4xspwkawcuzmx498 --height $pre_upgrade_height -o json | jq -r '."@type"')
 post_upgrade_acc_type=$(gaiad --home $HOME_1 q account cosmos145hytrc49m0hn6fphp8d5h4xspwkawcuzmx498 --height $post_upgrade_height -o json | jq -r '."@type"')
 
-echo "Pre upgrade account type is: pre_upgrade_acc_type"
+echo "Pre upgrade account type is: $pre_upgrade_acc_type"
 if [ "$pre_upgrade_acc_type" != "/cosmos.vesting.v1beta1.ContinuousVestingAccount" ]; then
     echo "Pre upgrade account is not a vesting account"
     exit 1
 fi
 
-echo "Post upgrade account type is: post_upgrade_acc_type"
+echo "Post upgrade account type is: $post_upgrade_acc_type"
 if [ "$post_upgrade_acc_type" != "/cosmos.auth.v1beta1.BaseAccount" ]; then
     echo "Post upgrade account is not a base account"
     exit 2
