f6be61bba63ad2a445b5bc8c28e68b9b588d2042
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-30 11:25:39 -0500

    skip jq parse

--- tests/v15_upgrade/flush_key_migration.sh
+++ tests/v15_upgrade/flush_key_migration.sh
@@ -6,4 +6,8 @@ $CHAIN_BINARY tx bank send $WALLET_1 $WALLET_1 1$DENOM --home $HOME_1 --from $WA
 $CHAIN_BINARY tx bank send $WALLET_2 $WALLET_2 1$DENOM --home $HOME_1 --from $WALLET_2 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
 $CHAIN_BINARY tx bank send $WALLET_3 $WALLET_3 1$DENOM --home $HOME_1 --from $WALLET_3 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
 $CHAIN_BINARY tx bank send $WALLET_4 $WALLET_4 1$DENOM --home $HOME_1 --from $WALLET_4 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+$CHAIN_BINARY tx bank send happy_bonding happy_bonding 1$DENOM --home $HOME_1 --from happy_bonding --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+$CHAIN_BINARY tx bank send happy_liquid_1 happy_liquid_1 1$DENOM --home $HOME_1 --from happy_liquid_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+$CHAIN_BINARY tx bank send happy_liquid_2 happy_liquid_2 1$DENOM --home $HOME_1 --from happy_liquid_2 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+$CHAIN_BINARY tx bank send happy_liquid_3 happy_liquid_3 1$DENOM --home $HOME_1 --from happy_liquid_3 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
 sleep $(($COMMIT_TIMEOUT+2))
