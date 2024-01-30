4606a341eabb8bc7f679bfe5a5ac2293a1935716
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-30 09:00:34 -0500

    post-upgrade LSM testing

--- tests/v15_upgrade/ica_delegate_happy.sh
+++ tests/v15_upgrade/ica_delegate_happy.sh
@@ -71,7 +71,7 @@ echo "** LIQUID STAKING PROVIDER HAPPY PATH> 2: DELEGATE VIA ICA **"
     cat delegate-happy-3.json
     echo "Generating packet JSON..."
     # $STRIDE_CHAIN_BINARY tx interchain-accounts host generate-packet-data "$(cat delegate-happy-3.json)" --encoding proto3json > delegate_packet.json # Stride v18
-    $STRIDE_CHAIN_BINARY tx interchain-accounts host generate-packet-data "$(cat delegate-happy-3.json)" > delegate_packet.json # Stride v18
+    $STRIDE_CHAIN_BINARY tx interchain-accounts host generate-packet-data "$(cat delegate-happy-3.json)" > delegate_packet.json # Stride v12
     echo "Sending tx staking delegate to host chain..."
     tests/v12_upgrade/log_lsm_data.sh lsp-happy pre-ica-delegate-1 $ICA_ADDRESS $delegate
     
