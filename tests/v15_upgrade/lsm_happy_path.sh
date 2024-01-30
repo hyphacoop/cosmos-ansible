690f99dd9ad11c518fec986b7b0529bbb282a41f
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-30 09:50:10 -0500

    tokenization count

--- tests/v15_upgrade/lsm_happy_path.sh
+++ tests/v15_upgrade/lsm_happy_path.sh
@@ -7,27 +7,27 @@ tokenize=50000000
 bank_send_amount=20000000
 ibc_transfer_amount=10000000
 liquid_1_redeem=20000000
-tokenized_denom=$VALOPER_1/1
+tokenized_denom=$VALOPER_1/$VALOPER_TOKENIZATION
 
-$CHAIN_BINARY keys add happy_bonding --home $HOME_1
-$CHAIN_BINARY keys add happy_liquid_1 --home $HOME_1
-$CHAIN_BINARY keys add happy_liquid_2 --home $HOME_1
-$CHAIN_BINARY keys add happy_liquid_3 --home $HOME_1
-$CHAIN_BINARY keys add happy_owner --home $HOME_1
+# $CHAIN_BINARY keys add happy_bonding --home $HOME_1
+# $CHAIN_BINARY keys add happy_liquid_1 --home $HOME_1
+# $CHAIN_BINARY keys add happy_liquid_2 --home $HOME_1
+# $CHAIN_BINARY keys add happy_liquid_3 --home $HOME_1
+# $CHAIN_BINARY keys add happy_owner --home $HOME_1
 
 
-happy_bonding=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_bonding").address')
-happy_liquid_1=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_1").address')
-happy_liquid_2=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_2").address')
-happy_liquid_3=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_3").address')
-happy_owner=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_owner").address')
+# happy_bonding=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_bonding").address')
+# happy_liquid_1=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_1").address')
+# happy_liquid_2=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_2").address')
+# happy_liquid_3=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_3").address')
+# happy_owner=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_owner").address')
 
-echo "Funding bonding and tokenizing accounts..."
-submit_tx "tx bank send $WALLET_1 $happy_bonding  200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
-submit_tx "tx bank send $WALLET_1 $happy_liquid_1 200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
-submit_tx "tx bank send $WALLET_1 $happy_liquid_2 200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
-submit_tx "tx bank send $WALLET_1 $happy_liquid_3 200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
-submit_tx "tx bank send $WALLET_1 $happy_owner 10000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+# echo "Funding bonding and tokenizing accounts..."
+# submit_tx "tx bank send $WALLET_1 $happy_bonding  200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+# submit_tx "tx bank send $WALLET_1 $happy_liquid_1 200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+# submit_tx "tx bank send $WALLET_1 $happy_liquid_2 200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+# submit_tx "tx bank send $WALLET_1 $happy_liquid_3 200000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
+# submit_tx "tx bank send $WALLET_1 $happy_owner 10000000uatom --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $HOME_1
 
 echo "** HAPPY PATH> STEP 1: VALIDATOR BOND **"
 
