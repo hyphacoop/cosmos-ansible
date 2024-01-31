322e700a773502c9ebf23358f3b4f303e68ba8ae
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-01-31 12:16:10 -0500

    start additional node

--- tests/v15_upgrade/test_min_commision.sh
+++ tests/v15_upgrade/test_min_commision.sh
@@ -3,30 +3,26 @@
 # Try creating a validator with a minimum commision of less than 5% before and after the upgrade
 
 if $UPGRADED_V15 ; then
-
+# Validator cannot be created with a minimum commission of less than the set by the param (5% for v15)
 
 else
-
+# Validator can be created with a min commission of 0%
+VAL_HOME=/home/runner/.mcval1
+$CHAIN_BINARY keys add mc_val1 --home $HOME_1
+mc_val=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="mc_val1").address')
+$CHAIN_BINARY tx bank send $WALLET_1 $mc_val 10000000$DENOM --home $HOME_1 --from $WALLET_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+sudo systemctl stop $PROVIDER_SERVICE_1
+cp -r $HOME_1 $VAL_HOME
+sudo systemctl start $PROVIDER_SERVICE_2
+$CHAIN_BINARY init dummy --home /home/runner/.dummykeys
+cp /home/runner/.dummykeys/config/priv_validator_key.json $VAL_HOME/config/priv_validator_key.json
+cp /home/runner/.dummykeys/config/node_key.json $VAL_HOME/config/node_key.json
+$CHAIN_BINARY tendermint unsafe-reset-all --home $VAL_HOME
+toml set --toml-path $VAL_HOME/config/app.toml api.address "tcp://0.0.0.0:25111"
+toml set --toml-path $VAL_HOME/config/app.toml grpc.address "0.0.0.0:28111"
+toml set --toml-path $VAL_HOME/config/config.toml rpc.laddr "tcp://0.0.0.0:27111"
+toml set --toml-path $VAL_HOME/config/config.toml rpc.pprof_laddr "localhost:29111"
+toml set --toml-path $VAL_HOME/config/config.toml p2p.laddr "tcp://0.0.0.0:26111"
+$CHAIN_BINARY start --home $VAL_HOME
 
 fi
-
-
-# Run a tx bank send with each of the keys in the keyring
-$CHAIN_BINARY keys list --home $HOME_1 --output json
-
-$CHAIN_BINARY tx bank send $WALLET_1 $WALLET_1 1$DENOM --home $HOME_1 --from $WALLET_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-$CHAIN_BINARY tx bank send $WALLET_2 $WALLET_2 1$DENOM --home $HOME_1 --from $WALLET_2 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-$CHAIN_BINARY tx bank send $WALLET_3 $WALLET_3 1$DENOM --home $HOME_1 --from $WALLET_3 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-$CHAIN_BINARY tx bank send $WALLET_4 $WALLET_4 1$DENOM --home $HOME_1 --from $WALLET_4 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-
-happy_bonding=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_bonding").address')
-happy_liquid_1=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_1").address')
-happy_liquid_2=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_2").address')
-happy_liquid_3=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_liquid_3").address')
-happy_owner=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="happy_owner").address')
-
-$CHAIN_BINARY tx bank send $happy_bonding $happy_bonding 1$DENOM --home $HOME_1 --from $happy_bonding --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-$CHAIN_BINARY tx bank send $happy_liquid_1 $happy_liquid_1 1$DENOM --home $HOME_1 --from $happy_liquid_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-$CHAIN_BINARY tx bank send $happy_liquid_2 $happy_liquid_2 1$DENOM --home $HOME_1 --from $happy_liquid_2 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-$CHAIN_BINARY tx bank send $happy_liquid_3 $happy_liquid_3 1$DENOM --home $HOME_1 --from $happy_liquid_3 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
-sleep $(($COMMIT_TIMEOUT+2))
