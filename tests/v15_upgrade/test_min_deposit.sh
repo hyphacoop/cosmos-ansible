c3b0ebf63b139f732bd55699a80ea07878b1514b
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-02-01 14:08:27 -0500

    fali if code 0 is not received on successful submission

--- tests/v15_upgrade/test_min_deposit.sh
+++ tests/v15_upgrade/test_min_deposit.sh
@@ -18,19 +18,25 @@ if $UPGRADED_V15 ; then
     fail_line=$(cat prop_response.log | grep "minimum deposit is too small")
     echo "$fail_line"
     if [[ -z "$fail_line" ]]; then
-        echo "FAIL: Validator creation did not output error with insufficient deposit"
+        echo "FAIL: Proposal submission did not output error with insufficient deposit"
         exit 1
     else
-        echo "PASS: Validator creation with insufficient outputs: $fail_line"
+        echo "PASS: Proposal submission with insufficient deposit fails: $fail_line"
     fi
 
-    sleep $(($COMMIT_TIMEOUT+2))
-
-    $CHAIN_BINARY tx gov submit-legacy-proposal --title="Test Proposal" --description="Test Proposal" \
+    code=$($CHAIN_BINARY tx gov submit-legacy-proposal --title="Test Proposal" --description="Test Proposal" \
     --type="Text" \
     --from $WALLET_1 --home $HOME_1 \
     --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
-    --deposit="1000000$DENOM" -y -o json
+    --deposit="1000000$DENOM" -y -o json)
+
+    if [[ "$code" == "0" ]]; then
+       echo "PASS: code 0 was received."
+    else
+       echo "FAIL: code 0 was not received."
+       exit 1
+    fi
+    sleep $(($COMMIT_TIMEOUT+2))
 else
 
     echo "Setting minimum deposit to 10ATOM..."
@@ -59,6 +65,7 @@ else
        echo "PASS: code 0 was received."
     else
        echo "FAIL: code 0 was not received."
+       exit 1
     fi
     sleep $(($COMMIT_TIMEOUT+2))
 fi
