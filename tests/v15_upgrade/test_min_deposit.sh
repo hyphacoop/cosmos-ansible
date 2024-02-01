0aadf9a704c647804051ea253b66a10789d54d2c
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-02-01 13:42:16 -0500

    submit-legacy-proposal after upgrade

--- tests/v15_upgrade/test_min_deposit.sh
+++ tests/v15_upgrade/test_min_deposit.sh
@@ -9,7 +9,7 @@ MCVAL_SERVICE_2=mcval2.service
 if $UPGRADED_V15 ; then
     echo "TEST: proposal submission deposit must be at least 10% of the minimum deposit."
 
-    $CHAIN_BINARY tx gov submit-proposal --title="Test Proposal" --description="Test Proposal" \
+    $CHAIN_BINARY tx gov submit-legacy-proposal --title="Test Proposal" --description="Test Proposal" \
     --type="Text" \
     --from $WALLET_1 --home $HOME_1 \
     --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
