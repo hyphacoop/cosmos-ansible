1801173d544e3891c0383463ba17e6e0988dcc39
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-02-01 15:55:58 -0500

    vote after upgrade

--- tests/v15_upgrade/test_voting.sh
+++ tests/v15_upgrade/test_voting.sh
@@ -3,7 +3,27 @@
 # Try creating a validator with a minimum commision of less than 5% before and after the upgrade
 
 if $UPGRADED_V15 ; then
+   voter1=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="voter1").address')
+   
+   output=$($CHAIN_BINARY tx gov submit-proposal --title="Test Proposal" --description="Test Proposal" \
+    --type="Text" \
+    --from $WALLET_1 --home $HOME_1 \
+    --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
+    --deposit="1000000$DENOM" -y -o json)
+   code=$(echo $output | jq '.code')
+   if [[ "$code" == "0" ]]; then
+      echo "PASS: code 0 was received."
+   else
+      echo "FAIL: code 0 was not received."
+      exit 1
+   fi
+   sleep $(($COMMIT_TIMEOUT+2))
+   txhash=$(echo $output | jq -r '.txhash')
+   proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+   echo "Proposal ID: $proposal_id"
    echo "TEST: Vote from an account with no delegations."
+   $CHAIN_BINARY tx gov vote $proposal_id yes --from $voter1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+   sleep $(($COMMIT_TIMEOUT+2))
 else
    $CHAIN_BINARY keys add voter1 --home $HOME_1
    voter1=$($CHAIN_BINARY keys list --home $HOME_1 --output json | jq -r '.[] | select(.name=="voter1").address')
@@ -29,7 +49,13 @@ else
    echo "Proposal ID: $proposal_id"
 
    echo "TEST: Vote from an account with no delegations."
-   $CHAIN_BINARY tx gov vote $proposal_id yes --from $voter1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json | jq '.'
+   code=$($CHAIN_BINARY tx gov vote $proposal_id yes --from $voter1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json | jq '.code')
+   if [[ "$code" == "0" ]]; then
+      echo "PASS: code 0 was received."
+   else
+      echo "FAIL: code 0 was not received."
+      exit 1
+   fi
    sleep $(($COMMIT_TIMEOUT+2))
 
 fi
