62a82d9d3f1c5df99e720c84f72c37c72ddb56a6
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-02-01 13:56:31 -0500

    check proposal response

--- tests/v15_upgrade/test_min_deposit.sh
+++ tests/v15_upgrade/test_min_deposit.sh
@@ -13,7 +13,22 @@ if $UPGRADED_V15 ; then
     --type="Text" \
     --from $WALLET_1 --home $HOME_1 \
     --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
-    --deposit="1000$DENOM" -y
+    --deposit="1000$DENOM" -y  >>prop_response.log 2>&1 
+    cat prop_response.log
+    fail_line=$(cat prop_response.log | grep "minimum deposit is too small")
+    echo "$fail_line"
+    if [[ -z "$fail_line" ]]; then
+        echo "FAIL: Validator creation did not output error with insufficient deposit"
+        exit 1
+    else
+        echo "PASS: Validator creation with insufficient outputs: $fail_line"
+    fi
+
+    $CHAIN_BINARY tx gov submit-legacy-proposal --title="Test Proposal" --description="Test Proposal" \
+    --type="Text" \
+    --from $WALLET_1 --home $HOME_1 \
+    --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
+    --deposit="1000000$DENOM" -y -o json
 else
 
     echo "Setting minimum deposit to 10ATOM..."
@@ -27,10 +42,20 @@ else
     --from $WALLET_1 --home $HOME_1 \
     --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
     --deposit="1000$DENOM" -y -o json | jq -r '.code')
-     if [[ "$code" == "3" ]]; then
-        echo "PASS: code 3 was received."
-     else
-        echo "FAIL: code 3 was not received."
-     fi
+    if [[ "$code" == "3" ]]; then
+       echo "PASS: code 3 was received."
+    else
+       echo "FAIL: code 3 was not received."
+    fi
 
+     code=$($CHAIN_BINARY tx gov submit-proposal --title="Test Proposal" --description="Test Proposal" \
+    --type="Text" \
+    --from $WALLET_1 --home $HOME_1 \
+    --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
+    --deposit="1000000$DENOM" -y -o json | jq -r '.code')
+    if [[ "$code" == "0" ]]; then
+       echo "PASS: code 0 was received."
+    else
+       echo "FAIL: code 0 was not received."
+    fi
 fi
