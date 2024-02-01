15ec6f367c9cdcc582fd0e2ffe5b244bfd649c50
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-02-01 13:17:16 -0500

    code 3 check

--- tests/v15_upgrade/test_min_deposit.sh
+++ tests/v15_upgrade/test_min_deposit.sh
@@ -6,26 +6,31 @@ MCVAL_HOME_2=/home/runner/.mcval2
 MCVAL_SERVICE_1=mcval1.service
 MCVAL_SERVICE_2=mcval2.service
 
-echo "TEST: proposal submission deposit must be at least 10% of the minimum deposit."
-params=$($CHAIN_BINARY q gov params --home $HOME_1 -o json | jq -r '.')
-echo $params
-
-tests/param_change.sh tests/v15_upgrade/proposal-10atom-deposit.json
-params=$($CHAIN_BINARY q gov params --home $HOME_1 -o json | jq -r '.')
-echo $params
-
-echo "Submitting proposal with < 10% of minimum deposit"
-
 if $UPGRADED_V15 ; then
+    echo "TEST: proposal submission deposit must be at least 10% of the minimum deposit."
+
     $CHAIN_BINARY tx gov submit-proposal --title="Test Proposal" --description="Test Proposal" \
     --type="Text" \
     --from $WALLET_1 --home $HOME_1 \
     --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
     --deposit="1000$DENOM" -y
 else
-    $CHAIN_BINARY tx gov submit-proposal --title="Test Proposal" --description="Test Proposal" \
+
+    echo "Setting minimum deposit to 10ATOM..."
+    tests/param_change.sh tests/v15_upgrade/proposal-10atom-deposit.json
+    params=$($CHAIN_BINARY q gov params --home $HOME_1 -o json | jq -r '.')
+    echo $params
+
+    echo "TEST: proposal submission deposit must be at least 10% of the minimum deposit."
+    code=$($CHAIN_BINARY tx gov submit-proposal --title="Test Proposal" --description="Test Proposal" \
     --type="Text" \
     --from $WALLET_1 --home $HOME_1 \
     --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
-    --deposit="1000$DENOM" -y
+    --deposit="1000$DENOM" -y -o json | jq -r '.code')
+     if [[ "$code" == "3" ]]; then
+        echo "PASS: code 3 was received."
+     else
+        echo "FAIL: code 3 was not received."
+     fi
+
 fi
