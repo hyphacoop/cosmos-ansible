c29ce68912207ed23318f8798b9a7c8019b2fc61
Dante <dante.a.sanchez@gmail.com>
2024-02-01 21:02:37 -0500

    restart relayer after upgrade

---
+++ tests/v15_upgrade/test_clawback.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+# set -e
+
+# Get upgrade height and time
+upgrade_height=$($CHAIN_BINARY --home $HOME_1 q upgrade applied v15 -o json | jq -r '.header.height')
+upgrade_time=$($CHAIN_BINARY --home $HOME_1 q upgrade applied v15 -o json | jq -r '.header.time')
+
+# Set heights
+let pre_upgrade_height=$upgrade_height-1
+let post_upgrade_height=$upgrade_height+1
+
+# Get account type from pre-upgrade and post-upgrade heights
+pre_upgrade_acc_type=$($CHAIN_BINARY --home $HOME_1 q account $CB_ACCT --height $pre_upgrade_height -o json | jq -r '."@type"')
+post_upgrade_acc_type=$($CHAIN_BINARY --home $HOME_1 q account $CB_ACCT --height $post_upgrade_height -o json | jq -r '."@type"')
+pre_upgrade_acc2_type=$($CHAIN_BINARY --home $HOME_1 q account $V_ACCT --height $pre_upgrade_height -o json | jq -r '."@type"')
+post_upgrade_acc2_type=$($CHAIN_BINARY --home $HOME_1 q account $V_ACCT --height $post_upgrade_height -o json | jq -r '."@type"')
+
+# Check if account type matches
+echo "Pre upgrade account type is: $pre_upgrade_acc_type"
+if [ "$pre_upgrade_acc_type" != "/cosmos.vesting.v1beta1.ContinuousVestingAccount" ]; then
+    echo "Pre upgrade account is not a vesting account"
+    exit 1
+fi
+
+echo "Post upgrade account type is: $post_upgrade_acc_type"
+if [ "$post_upgrade_acc_type" != "/cosmos.auth.v1beta1.BaseAccount" ]; then
+    echo "Post upgrade account is not a base account"
+    exit 2
+fi
+
+# Check if account type matches
+echo "Pre upgrade account2 type is: $pre_upgrade_acc_type"
+if [ "$pre_upgrade_acc2_type" != "/cosmos.vesting.v1beta1.ContinuousVestingAccount" ]; then
+    echo "Pre upgrade account is not a vesting account"
+    exit 3
+fi
+
+echo "Post upgrade account type is: $post_upgrade_acc_type"
+if [ "$post_upgrade_acc2_type" != "/cosmos.vesting.v1beta1.ContinuousVestingAccount" ]; then
+    echo "Post upgrade account is not a vesting account, this account's type shouldn't change"
+    exit 4
+fi
+
+echo "Spendable balances for $CB_ACCT must not increase."
+# Get spendable balances 1
+spendable_balance1_acc=$(gaiad --home $HOME_1 q bank spendable-balances $CB_ACCT -o json | jq -r '.balances[] | select(.denom == "uatom") | .amount')
+echo "Current spendable balance is: $spendable_balance1_acc"
+
+sleep $VOTING_PERIOD
+
+# Get spendable balances 2
+spendable_balance2_acc=$(gaiad --home $HOME_1 q bank spendable-balances $CB_ACCT -o json | jq -r '.balances[] | select(.denom == "uatom") | .amount')
+echo "Current spendable balance is: $spendable_balance2_acc"
+
+# Check if spendable balance matches
+if [ $spendable_balance1_acc -ne $spendable_balance2_acc ]; then
+    echo "Spendable balance 1 is: $spendable_balance1_acc balance 2 is: $spendable_balance2_acc"
+    exit 5
+else
+    echo "Spendable balance unchanged as expected, spendable balance 1 is: $spendable_balance1_acc balance 2 is: $spendable_balance2_acc"
+fi
+
+echo "Spendable balances for $V_ACCT must increase."
+# Get spendable balances 1
+spendable_balance1_acc2=$(gaiad --home $HOME_1 q bank spendable-balances $V_ACCT -o json | jq -r '.balances[] | select(.denom == "uatom") | .amount')
+echo "Current spendable balance is: $spendable_balance1_acc2"
+
+sleep $VOTING_PERIOD
+
+# Get spendable balances 2
+spendable_balance2_acc2=$(gaiad --home $HOME_1 q bank spendable-balances $V_ACCT -o json | jq -r '.balances[] | select(.denom == "uatom") | .amount')
+echo "Current spendable balance is: $spendable_balance2_acc2"
+
+# Check if spendable balance matches
+if [ $spendable_balance1_acc2 -ne $spendable_balance2_acc2 ]; then
+    echo "Spendable balance 1 is: $spendable_balance1_acc2 balance 2 is: $spendable_balance2_acc2"
+else
+    echo "Spendable unchanged, spendable balance 1 is: $spendable_balance1_acc2 balance 2 is: $spendable_balance2_acc2"
+    exit 6
+fi
+
+# Check bank balances
+echo "Spendable balances acc 1"
+echo "Before upgrade"
+$CHAIN_BINARY --home $HOME_1 q bank spendable-balances $CB_ACCT --height $pre_upgrade_height
+
+echo "After upgrade"
+$CHAIN_BINARY --home $HOME_1 q bank spendable-balances $CB_ACCT --height $post_upgrade_height
+
+echo "Spendable balances acc 2"
+echo "Before upgrade"
+$CHAIN_BINARY --home $HOME_1 q bank spendable-balances $V_ACCT --height $pre_upgrade_height
+
+echo "After upgrade"
+$CHAIN_BINARY --home $HOME_1 q bank spendable-balances $V_ACCT --height $post_upgrade_height
+
+# Check community pool
+pre_upgrade_cp=$($CHAIN_BINARY --home $HOME_1 q distribution community-pool --height $pre_upgrade_height -o json | jq -r '.pool[] | select(.denom == "uatom") | .amount')
+echo "Community pool balance before upgrade: $pre_upgrade_cp"
+
+post_upgrade_cp=$($CHAIN_BINARY --home $HOME_1 q distribution community-pool --height $post_upgrade_height -o json | jq -r '.pool[] | select(.denom == "uatom") | .amount')
+echo "Community pool balance after upgrade: $post_upgrade_cp"
+
+cp_diff=$(echo "$post_upgrade_cp-$pre_upgrade_cp" | bc -l)
+
+echo "Community pool differences: $cp_diff"
+
+if [ $(bc -l <<< "$post_upgrade_cp < 100000000") -eq 1 ]
+then
+    echo "Community pool balance is less than 100000000uatom, funds did not returned from wallet"
+    exit 7
+else
+    echo "Community pool balance is more than 100000000uatom, funds have been returned"
+fi
+
+# get the block time of the tx for the vesting account
+vesting_txhash=$(echo $TX_VESTING_ACC_TX_JSON | jq -r .txhash)
+echo "Vesting  account create txhash: $vesting_txhash"
+vesting_height=$(gaiad --home $HOME_1 q tx $vesting_txhash -o json | jq -r '.height')
+echo "Vesting height: $vesting_height"
+vesting_block_time=$(gaiad --home $HOME_1 q block $vesting_height | jq -r '.block.header.time')
+vesting_epoch=$(date -d "$vesting_block_time" +%s)
+upgrade_epoch=$(date -d "$upgrade_time" +%s)
+
+echo "Vesting account started at: $vesting_block_time epoch: $vesting_epoch"
+echo "upgrade time: $upgrade_time epoch: $upgrade_epoch"
+
+# Calculate vesting duration
+vesting_duration=$(echo "1770000000-$vesting_epoch" | bc -l)
+echo "Total vesting duration: $vesting_duration"
+
+# Calculate elapsed vesting
+vesting_elapsed=$(echo "$upgrade_epoch-$vesting_epoch" | bc -l)
+echo "Elapsed vesting: $vesting_elapsed"
+
+# Calucate vesting amount
+vesting_div=$(echo "$vesting_elapsed/$vesting_duration" | bc -l)
+vested=$(echo "100000000*$vesting_div")
+echo "Vested amount: $vested"
+
+unvested=$(echo "100000000-$vested" | bc -l)
+echo "Unvested amount: $unvested"
