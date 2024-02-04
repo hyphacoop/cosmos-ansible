6347cf6005d160276c6607cddce38c44842ab2c3
Dante <dante.a.sanchez@gmail.com>
2024-02-03 20:45:57 -0500

    happy path voting test

--- tests/v15_upgrade/test_voting.sh
+++ tests/v15_upgrade/test_voting.sh
@@ -61,12 +61,44 @@ if $UPGRADED_V15 ; then
    sleep $(($COMMIT_TIMEOUT+2))
    code=$($CHAIN_BINARY q tx $txhash --home $HOME_1 -o json | jq '.code')
    echo "Vote tx code: \"$code\""
-   echo "Vote tx code: \"$code\""
    if [[ "$code" == "0" ]]; then
       echo "FAIL: code 0 was received."
       exit 1
    else
       echo "PASS: code 0 was not received."
+   fi
+   sleep $VOTING_PERIOD
+   $CHAIN_BINARY q gov proposal $proposal_id --home $HOME_1 -o json | jq '.'
+
+   $CHAIN_BINARY tx staking delegate $VALOPER_1 1000000$DENOM --from $voter1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json
+   sleep $(( $COMMIT_TIMEOUT*2 ))
+   $CHAIN_BINARY q staking delegations $voter1 --home $HOME_1 -o json | jq '.'
+   output=$($CHAIN_BINARY tx gov submit-legacy-proposal --title="Test Proposal" --description="Test Proposal" \
+    --type="Text" \
+    --from $WALLET_1 --home $HOME_1 \
+    --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM \
+    --deposit="1000000$DENOM" -y -o json)
+   code=$(echo $output | jq '.code')
+   if [[ "$code" == "0" ]]; then
+      echo "PASS: code 0 was received."
+   else
+      echo "FAIL: code 0 was not received."
+      exit 1
+   fi
+   sleep $(($COMMIT_TIMEOUT+2))
+   txhash=$(echo $output | jq -r '.txhash')
+   proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+   echo "Proposal ID: $proposal_id"
+   echo "TEST: Vote from an account with 1_000_001uatom in delegations."
+   txhash=$($CHAIN_BINARY tx gov vote $proposal_id yes --from $voter1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json | jq -r '.txhash')
+   sleep $(($COMMIT_TIMEOUT+2))
+   code=$($CHAIN_BINARY q tx $txhash --home $HOME_1 -o json | jq '.code')
+   echo "Vote tx code: \"$code\""
+   if [[ "$code" == "0" ]]; then
+      echo "PASS: code 0 was received."
+   else
+      echo "FAIL: code 0 was not received."
+      exit 1
    fi
    sleep $VOTING_PERIOD
    $CHAIN_BINARY q gov proposal $proposal_id --home $HOME_1 -o json | jq '.'
