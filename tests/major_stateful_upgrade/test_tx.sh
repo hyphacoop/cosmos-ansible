85797bfceebdbc621906e72221ee70571ec87b65
ASoTNetworks <dev@aseriesoftubez.com>
2024-02-05 15:57:13 -0500

    migration lines

--- tests/major_stateful_upgrade/test_tx.sh
+++ tests/major_stateful_upgrade/test_tx.sh
@@ -32,7 +32,6 @@ check_code()
 }
 
 echo "Sending funds with tx bank send..."
-$CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 $VAL_STAKE_STEP$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID
 TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 $VAL_STAKE_STEP$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b sync | jq '.txhash' | tr -d '"')
 sleep 20
 check_code $TXHASH
