4dc6eefdfd106187000d2a72bf71019ec404e887
ASoTNetworks <dev@aseriesoftubez.com>
2024-02-05 19:42:37 -0500

    check if slashing tx is commited to block

--- tests/major_stateful_upgrade/test_pupmos_slashing.sh
+++ tests/major_stateful_upgrade/test_pupmos_slashing.sh
@@ -5,57 +5,25 @@ submit_proposal_cmd="gaiad --home $HOME_1 tx provider submit-consumer-double-vot
 echo "Running: $submit_proposal_cmd"
 TXHASH=$($submit_proposal_cmd | jq '.txhash' | tr -d '"')
 
-sleep 90
 echo "Transection TX hash is: $TXHASH"
-gaiad --home $HOME_1 q tx $TXHASH
 
-exit 1 
-while [ $try -lt 5 ]; do
-    code=$($CHAIN_BINARY q tx $txhash -o json --home $HOME_1 | jq '.code')
-    echo "Code is: $code"
-    if [ -z $code ]; then
-        echo "code returned blank, tx was unsuccessful. Try: $try"
-        let try=$try+1
-        sleep 20
-    elif [ $code -ne 0 ]; then
-        echo "code returned not 0, tx was unsuccessful. Try: $try"
-        let try=$try+1
-        sleep 20
-    else
-        echo "tx was successful"
-        break
-    fi
-done
-if [ $try -gt 4 ]; then
-    echo "maximum query reached tx unsuccessful."
-    #$CHAIN_BINARY q tx $txhash -o json --home $HOME_1 | jq '.'
-    exit 1
-fi
+# wait for 3 blocks
+echo "Waiting for 3 blocks to be comitted"
+tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 3 10
 
-if [ $exit_code -eq 0 ]
-then
-    echo "ERROR: submit-consumer-double-voting submitted successfully..."
-    submit_successful=1
-fi
+echo "Query txhash:"
+gaiad --home $HOME_1 q tx $TXHASH -o json --home $HOME_1 | jq '.'
 
-echo "Waiting for tx to be applied to block"
-sleep 10
-
-echo "Querying slashing result"
-slashing_result=$(gaiad --home $HOME_1 q slashing signing-info '{"@type":"/cosmos.crypto.ed25519.PubKey","key":"0Nu7qOxlaLNKgOKM2Ck0fB6aKdsH1tjOeJw18VtES2g="}' -o json | jq -r '.tombstoned')
-
-echo "PUPMOS slashing result: $slashing_result"
-
-if [ "$slashing_result" != "false" ]
-then
-    echo "PUPMOS slashing result is not false"
-    slashing_not_false=1
-fi
-
-if [ $submit_successful ] || [ $slashing_not_false ]
-then
-    echo "PUPMOS test failed!"
-    exit 1
-else
+code=$($CHAIN_BINARY q tx $txhash -o json --home $HOME_1 | jq '.code')
+echo "Code is: $code"
+if [ -z $code ]; then
+    echo "code returned blank, tx was unsuccessful."
     echo "PUPMOS is safe!"
+elif [ $code -ne 0 ]; then
+    echo "code returned not 0, tx was unsuccessful."
+    echo "PUPMOS is safe!"
+else
+    echo "tx was successful"
+    echo "PUPMOS slashing was commited to a block!"
+    exit 1
 fi
