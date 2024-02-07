1fff705527e1ec608ffb8b8997a7661d5f5f9e8c
ASoTNetworks <dev@aseriesoftubez.com>
2024-02-07 12:08:51 -0500

    patch ccv for backwards compatibility

--- tests/major_stateful_upgrade/launch_consumer.sh
+++ tests/major_stateful_upgrade/launch_consumer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Launch a consumer chain
 
+# $1 sets the backwards compatibility version
+
+transform=$1
+
 echo "Patching add template with spawn time..."
 spawn_time=$(date -u --iso-8601=ns | sed s/+00:00/Z/ | sed s/,/./)
 jq -r --arg SPAWNTIME "$spawn_time" '.spawn_time |= $SPAWNTIME' tests/patch_upgrade/proposal-add-template.json > proposal-add-spawn.json
@@ -34,7 +38,7 @@ $CHAIN_BINARY q gov tally $proposal_id --home $HOME_1
 
 echo "Waiting for proposal to pass..."
 sleep $VOTING_PERIOD
-tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 3 10
+tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
 
 #$CHAIN_BINARY q gov proposals --home $HOME_1
 
@@ -42,6 +46,15 @@ echo "Collecting the CCV state..."
 $CHAIN_BINARY q provider consumer-genesis $CONSUMER_CHAIN_ID -o json --home $HOME_1 > ccv-pre.json
 jq '.params |= . + {"soft_opt_out_threshold": "0.05"}' ccv-pre.json > ccv.json
 jq '.' ccv.json
+
+if [ -z $transform ]
+then
+    echo "Patching CCV for backwards compatibility"
+    wget https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v3.3.0-transform/interchain-security-cd -O ics-transform
+    chmod +x ics-transform
+    ./ics-transform genesis transform --to $transform ccv.json > ccv-transform.json
+    cp ccv-transform.json ccv.json
+fi
 
 echo "Patching the consumer genesis file..."
 jq -s '.[0].app_state.ccvconsumer = .[1] | .[0]' $CONSUMER_HOME_1/config/genesis.json ccv.json > consumer-genesis.json
