4d05e04bac5da089e4185820fc88168cc34aa1bd
ASoTNetworks <dev@aseriesoftubez.com>
2024-03-20 21:53:47 -0400

    fix file perm

--- tests/export_genesis.sh
+++ tests/export_genesis.sh
@@ -125,9 +125,11 @@ echo `date +"%b %d %H:%M:%S "` "Installed gaiad version is $chain_version"
 if [ ! -d mainnet-genesis-export ]
 then
     mkdir mainnet-genesis-export
+    chown
 fi
 echo `date +"%b %d %H:%M:%S "` "Export genesis"
-time su gaia -c "~gaia/.gaia/cosmovisor/current/bin/gaiad export --height $current_block --output-document mainnet-genesis-export/mainnet-genesis_${current_block_time}_${chain_version}_${current_block}.json"
+time su gaia -c "~gaia/.gaia/cosmovisor/current/bin/gaiad export --height $current_block --output-document /tmp/genesis_export.json
+mv /tmp/genesis_export.json mainnet-genesis-export/mainnet-genesis_${current_block_time}_${chain_version}_${current_block}.json"
 
 echo `date +"%b %d %H:%M:%S "` "Tinkering exported genesis"
 pip3 install -r requirements.txt
