eebe97caa8baa2475a5a450dce5ff216c9ebf16e
ASoTNetworks <dev@aseriesoftubez.com>
2025-11-12 15:54:37 -0500

    update gaia versions / statesync script

--- roles/node/files/get_trust_height.sh
+++ roles/node/files/get_trust_height.sh
@@ -12,5 +12,5 @@ else
 fi
 
 LATEST_HEIGHT=$(curl -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0" -s $1/block | jq -r .result.block.header.height)
-BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL))
+BLOCK_HEIGHT=$(echo $LATEST_HEIGHT | awk '{printf "%d000-1000\n", $0 / 1000}' | bc)
 echo -n $BLOCK_HEIGHT
